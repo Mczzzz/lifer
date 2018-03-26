f948cd210b3a16c8a2f95df404f48ac85e10a6e7
g.bailly <g.bailly@immanens.coms>
2018-03-26 16:25:16 +0200

    Ajout du controller pour la node

---
+++ src/AppBundle/Controller/NodeController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class NodeController extends Controller
+{
+
+
+    /**
+     * @Route("/api/node", name="api_node")
+     * @Method("POST")
+     */
+
+    public function NodeManager(Request $request)
+    {
+
+        // is it an Ajax request?
+        $isAjax = $request->isXmlHttpRequest();
+
+        $user = $this->getUser();
+
+        var_dump($user);
+        die();
+
+
+        $request = Request::createFromGlobals();
+
+        $tmpDir = $this->getParameter('tmpDir');
+
+        $file = $request->files->get('jpg')->getClientOriginalName();
+
+        $path_parts = pathinfo($file);
+
+        $request->files->get('jpg')->move($tmpDir,$path_parts['basename']);
+
+
+        return new Response('OK');
+
+    }       
+
+
+
+
+
+
+}
