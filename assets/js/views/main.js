eccd4f717ed777da1173997a4a7f0b199e78894d
g.bailly <g.bailly@immanens.coms>
2018-03-28 18:21:43 +0200

    maj app

--- assets/js/views/main.js
+++ assets/js/views/main.js
@@ -3,7 +3,7 @@ export default class Main {
 
 	constructor(){
 
-		this.app = new PIXI.Application();  // Add the view to the DOM 		
+		this.app = new PIXI.Application(window.innerWidth, window.innerHeight, { backgroundColor : 0x000000, antialias : true });  // Add the view to the DOM 		
 
 		document.body.appendChild(this.app.view);  // ex, add display objects 
 
