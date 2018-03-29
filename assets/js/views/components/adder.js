b4cd8152d40f27414b5ff196c8379604f2f909af
g.bailly <g.bailly@immanens.coms>
2018-03-29 15:57:31 +0200

    Mise a jour bouton Tap

--- assets/js/views/components/adder.js
+++ assets/js/views/components/adder.js
@@ -4,12 +4,14 @@ export default class Adder {
 	constructor(rayonButton){
 
 		this.container = new PIXI.Container();
+		this.container.interactive = true;
 
 		this.rayonButton = rayonButton;
 
 		this.drawShadow();
 		this.drawCircle();
 		this.drawPlus();
+		this.addEventTap();
 
 		return this.container;
 
@@ -57,12 +59,28 @@ export default class Adder {
 
 	drawPlus(){
 
-		this.plus = new PIXI.Text("+",{fontFamily : 'Arial', fontSize: 50,fill : 0xFFFFFF, align : 'center'});
+		this.plus = new PIXI.Text("+",{fontFamily : 'Arial', fontSize: 25,fill : 0xFFFFFF, align : 'center'});
         this.plus.x = this.rayonButton / 2;
         this.plus.y = this.rayonButton / 2 - this.plus.width / 2;
 
         this.container.addChild(this.plus);
 
 	}
+
+
+	addEventTap(){
+
+
+		this.container.on('tap', (event) => {
+		  
+		  alert('test Event tap');
+
+		});
+
+
+
+
+	}
+
 
 }
