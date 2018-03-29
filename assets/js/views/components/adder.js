934fd9dd29ca7a286b26e96848355a57fe93b22e
g.bailly <g.bailly@immanens.coms>
2018-03-29 15:07:55 +0200

    Mise a jour bouton

--- assets/js/views/components/adder.js
+++ assets/js/views/components/adder.js
@@ -51,11 +51,17 @@ export default class Adder {
 		this.container.addChild(this.spriteShadow);
 
 
-
-
-
-
 	}
 
+
+	drawPlus(){
+
+		this.plus = new PIXI.Text("+",{fontFamily : 'Arial', fontSize: 50,fill : 0xFFFFFF, align : 'center'});
+        this.plus.x = this.rayonButton / 2 + this.plus.width / 2;
+        this.plus.y = this.rayonButton / 2
+
+        this.container.addChild(this.plus);
+
+	}
 
 }
