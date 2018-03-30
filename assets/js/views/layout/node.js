b2bcba9e4e6540200b3dce9c054d67432bab864f
g.bailly <g.bailly@immanens.coms>
2018-03-30 21:01:54 +0200

    Node add image

--- assets/js/views/layout/node.js
+++ assets/js/views/layout/node.js
@@ -42,13 +42,57 @@ export default class Node {
 
 	addImage(){
 
-		this.pict = new PIXI.Sprite.fromImage(this.data);
+		//trop moche à revoir car un load double de l'image
+		let i = new Image();
+		i.onload = function(){
+
+            let ratio = i.height / i.width;
+
+            this.pict = new PIXI.Sprite.fromImage(this.data);
+			pict.interactive = true;
+             
+            this.pict.height = this.pict.width * ratio;
+             
+            let diffsize =  this.pict.height - this.pict.width;
+
+            this.pict.anchor.set(0.5,0.5);
+
+     		this.pict.x = 2.5;
+			this.pict.y = 2.5;
+
+            this.pict.rotation = Math.PI * 2 * 0.25;
+            this.pict.height = window.innerWidth - 120;
+            this.pict.width = this.pict.height / ratio;
+            this.pict.y -= diffsize - 25;
+             // pict.pivot.set(pict.width, pict.height);
+              
+              //pict.rotation = 1.5702;
+             // pict.rotation = 0.5;
+             //console.log(pict);
+
+            this.pict.on('tap', (event) => {
+
+             	this.pict.rotation += Math.PI * 2 * 0.25;
+             	
+            });
+
+
+
+/*
+
+
 		console.log(this.pict.width + " / " + this.pict.height);
 		this.pict.width = this.width - 5;
 		this.pict.x = 2.5;
-		this.pict.y = 2.5;
+		this.pict.y = 2.5;*/
 
-		this.container.addChild(this.pict);
+			this.container.addChild(this.pict);
+
+
+		};
+		i.src = this.data;
+
+
 
 	}
 
