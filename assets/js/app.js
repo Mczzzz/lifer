dba116078d2441cece9c9b742e44f4e844400d21
g.bailly <g.bailly@immanens.coms>
2018-04-01 23:07:37 +0200

    Travail sur le header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js';
-import moment from 'moment/moment.js';
+import moment from 'moment-es6';
 import Main from './controller/main.js';
 
 //loader de Frame via custom Events
