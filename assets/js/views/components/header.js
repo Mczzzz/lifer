50158530986b72f830612fedb0b18c9efa5a08a7
g.bailly <g.bailly@immanens.coms>
2018-04-01 22:40:52 +0200

    Centrage de l'image

--- assets/js/views/components/header.js
+++ assets/js/views/components/header.js
@@ -29,10 +29,8 @@ export default class Header {
 		let rect = new Rect(0xE8E8E8 , 0, 0 , 0 , window.innerWidth , 50);
 		this.attach(rect);
 
-
-
-
-
+		let text = new PIXI.Text(moment().format('LLLL'),{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+		this.attach(text);
 	}
 
 
