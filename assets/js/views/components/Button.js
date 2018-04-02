55afb4c2968dada5da2794af8a5e845640664339
g.bailly <g.bailly@immanens.coms>
2018-04-03 01:13:20 +0200

    Integration Button

--- assets/js/views/components/Button.js
+++ assets/js/views/components/Button.js
@@ -23,8 +23,8 @@ export default class Button extends UIContainer{
 		console.log(this.container.y);
 		console.log(this.width);
 		console.log(this.height);
-		let rect = new Rect(0xFF99FF, 0, this.container.x , this.container.y , this.width , this.height);
-		
+		//let rect = new Rect(0xFF99FF, 0, this.container.x , this.container.y , this.width , this.height);
+		let rect = new Rect(0xFF99FF, 0, 0 , 0 , 100 , 100);
 
 /*				let rect = new PIXI.Graphics();
         rect.beginFill(0xFF99FF);
@@ -34,7 +34,7 @@ export default class Button extends UIContainer{
 */
 
 
-
+		
 		this.attach(rect);
 
 
