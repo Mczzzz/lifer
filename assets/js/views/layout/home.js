378380735031a0e77dd115a4c96b7dd72e1ebd95
g.bailly <g.bailly@immanens.coms>
2018-04-02 10:46:23 +0200

    Integration momentjs

--- assets/js/views/layout/home.js
+++ assets/js/views/layout/home.js
@@ -44,7 +44,7 @@ export default class Home {
 	drawBkground(){
 
 		this.backgroundCard = new PIXI.Graphics();
-        this.backgroundCard.beginFill(0xFFFFFF);
+        this.backgroundCard.beginFill(0xD8D8D8);
         this.backgroundCard.lineStyle(0);
         this.backgroundCard.drawRect(50, 50, window.innerWidth - 100, window.innerHeight - 100);
         this.backgroundCard.endFill();
