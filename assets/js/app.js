a37fd3349f36b7c1f3f4d34032790a4148ab5025
g.bailly <g.bailly@immanens.coms>
2018-04-03 15:15:13 +0200

    addHeader Shadow

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,12 @@ import Main from './controller/main.js';
 //loader de Frame via custom Events
 //console.log(moment(123456).fromNow());
 
+navigator.geolocation.getCurrentPosition(function(location) {
+  console.log(location.coords.latitude);
+  console.log(location.coords.longitude);
+  console.log(location.coords.accuracy);
+});
+
+
 const LiferApp = new Main();
 
