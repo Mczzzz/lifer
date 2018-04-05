65703251a87bebd8bd9cd124df00d4a0ae958bb7
g.bailly <g.bailly@immanens.coms>
2018-04-05 06:14:14 +0200

    BACK-END : Ajout de la mehode getAllNode

--- src/AppBundle/Controller/NodeController.php
+++ src/AppBundle/Controller/NodeController.php
@@ -74,5 +74,59 @@ class NodeController extends Controller
 
 
 
+ /**
+     * @Route("/api/node", name="api_node")
+     * @Method("GET")
+     */
+
+    public function NodeGetList(Request $request)
+    {
+
+        // is it an Ajax request?
+        //$isAjax = $request->isXmlHttpRequest();
+
+        //users infos
+        $user = $this->getUser();
+
+
+        //interception de la requete
+       // $request = Request::createFromGlobals();
+        //$text = $request->request->get('text');
+
+        // j'enregistre en base ma note
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $list = $entityManager->getRepository('Node')->findAll();
+
+        foreach($list as $item){
+
+
+
+        }
+        $response = new \stdClass();
+        $response->status = true;
+        $response->nb = count($list);
+/*
+        $request = Request::createFromGlobals();
+
+        $tmpDir = $this->getParameter('tmpDir');
+
+        $file = $request->files->get('jpg')->getClientOriginalName();
+
+        $path_parts = pathinfo($file);
+
+        $request->files->get('jpg')->move($tmpDir,$path_parts['basename']);
+
+*/
+        return new Response($response);
+
+    }       
+
+
+
+
+
+
+
 
 }
