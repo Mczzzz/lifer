f53256d728421d9d904e85fb721d7bfcb38af7ab
g.bailly <g.bailly@immanens.coms>
2018-04-06 15:16:03 +0200

    Mise a jour list button

---
+++ assets/js/views/elements/RoundedRect.js
@@ -0,0 +1,29 @@
+export default class RoundedRect {
+
+
+	constructor(color , liStyle, round, x , y , width , height, alpha){
+
+
+
+		let element = this.drwRect(color , liStyle, x , y , width , height, alpha);
+
+		return element;
+	}
+
+
+	drwRect(color , liStyle, x , y , width , height, round, alpha){
+
+		let rect = new PIXI.Graphics();
+        rect.beginFill(color);
+        rect.lineStyle(liStyle);
+        rect.drawRoundedRect(x, y, width, height, round);
+        rect.endFill();
+
+        rect.fillAlpha = alpha;
+        
+        console.log('on renvoi le rectangle');
+		return rect; 
+
+	}
+
+}
