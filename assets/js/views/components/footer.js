4e4bfc83d1a37ca539ab1f4ca232547b9f7f59f0
g.bailly <g.bailly@immanens.coms>
2018-04-09 17:04:11 +0200

    Mise a Tree Link elements

--- assets/js/views/components/footer.js
+++ assets/js/views/components/footer.js
@@ -71,8 +71,8 @@ export default class Footer  extends UIContainer{
 		this.loader.add('addTrigger', 'assets/glyphs/trigger.svg');
 
 		this.loader.load((loader, resources) => {
-     		this.addNode = new Icon(72,50 ,resources.addNode.texture, 0xFFFFFF);
-     		this.TheAddNode = this.addNode.load();
+     		this.ANode = new Icon(72,50 ,resources.addNode.texture, 0xFFFFFF);
+     		this.TheAddNode = this.ANode.load();
 
 			this.TheAddNode.on('tap', (event) => {
 				let NodeEvent = new CustomEvent('changeFrame', {'detail' : {'frame' : 'node'}});
@@ -80,16 +80,16 @@ export default class Footer  extends UIContainer{
 			});
 
 
-     		this.addLinker = new Icon(216,50 ,resources.addLinker.texture, 0xFFFFFF);
-     		this.TheAddLinker = this.addLinker.load();
+     		this.ALinker = new Icon(216,50 ,resources.addLinker.texture, 0xFFFFFF);
+     		this.TheAddLinker = this.ALinker.load();
 			
 			this.TheAddLinker.on('tap', (event) => {
 				let LinkEvent = new CustomEvent('changeFrame', {'detail' : {'frame' : 'link'}});
                 window.dispatchEvent(LinkEvent);
 			});
 
-     		this.addTrigger = new Icon(360,50 ,resources.addTrigger.texture, 0xFFFFFF);
-/**/		this.TheAddTrigger = this.addTrigger.load();
+     		this.ATrigger = new Icon(360,50 ,resources.addTrigger.texture, 0xFFFFFF);
+/**/		this.TheAddTrigger = this.ATrigger.load();
 
 
 /*     		this.realSizeX = resources.addNode.texture.baseTexture.realWidth;
