bf1fb433cf50607e07e4b92c5226c40df1713bb7
Guillaume Bailly <g.bailly@immanens.com>
2018-05-06 16:03:42 +0200

    j'ajoute la table de mon arbre interne

---
+++ src/AppBundle/Entity/Humans_tree.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Objects_infos
+ *
+ * @ORM\Table(name="humans_tree")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Humans_treeRepository")
+ */
+class Humans_tree
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="object", type="integer", nullable=false)
+     */
+    private $object;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="update", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+     */
+    private $update;
+
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="parent", type="integer", nullable=true)
+     */
+    private $parent;
+
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="creator", type="integer", nullable=false)
+     */
+    private $creator;
+
+    
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Objects_infos
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+   
+    /**
+     * Set object
+     *
+     * @param integer $object
+     *
+     * @return Objects_infos
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    /**
+     * Get object
+     *
+     * @return integer
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+
+
+    /**
+     * Set update
+     *
+     * @param datetime $update
+     *
+     * @return Updates_infos
+     */
+    public function setUpdate($update)
+    {
+        $this->update = $update;
+
+        return $this;
+    }
+
+    /**
+     * Get update
+     *
+     * @return datetime
+     */
+    public function getUpdate()
+    {
+        return $this->update;
+    }
+
+
+
+    /**
+     * Set parent
+     *
+     * @param integer $parent
+     *
+     * @return Parents_infos
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return integer
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+
+    /**
+     * Set creator
+     *
+     * @param integer $creator
+     *
+     * @return Objects
+     */
+    public function setCreator($creator)
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get creator
+     *
+     * @return int
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+
+
+
+
+}
+
