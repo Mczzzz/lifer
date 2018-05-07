63e6314f22f16e429a33820e431fc7183ad163fb
g.bailly <g.bailly@immanens.coms>
2018-05-07 11:16:56 +0200

    Mise à jour de l'entité UsersTree

--- src/AppBundle/Entity/UsersTree.php
+++ src/AppBundle/Entity/UsersTree.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="users_tree")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UsersTreeRepository")
  */
-class UserTree
+class UsersTree
 {
     /**
      * @var int
