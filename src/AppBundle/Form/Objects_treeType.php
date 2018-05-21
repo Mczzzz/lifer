7468e1601c5a6c1f7d45a4391ca7e867c7178a2f
Guillaume Bailly <g.bailly@immanens.com>
2018-05-21 17:00:54 +0200

    Mise a jour de object tree form pour gerer les objects associés - debug j'ai oublié les use trop con le gars

--- src/AppBundle/Form/Objects_treeType.php
+++ src/AppBundle/Form/Objects_treeType.php
@@ -6,6 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use AppBundle\Entity\User;
+use AppBundle\Entity\Objects;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 class Objects_treeType extends AbstractType
 {
     /**
