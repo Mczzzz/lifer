e04a6a3ab56db3b0e90fe29a961a37e326b0d3ec
g.bailly <g.bailly@immanens.com>
2018-05-31 14:09:20 +0200

    finalisation du dnd sur le deuxieme tableau

--- src/AppBundle/Controller/JsTreeController.php
+++ src/AppBundle/Controller/JsTreeController.php
@@ -92,6 +92,56 @@ class JsTreeController extends Controller
     }
 
 
+
+
+    /**
+     * @Route("/object/tree/dnd", name="node_dnd")
+     * @Method("POST")
+     */
+    public function MoveAction(Request $request)
+    {
+        $request = Request::createFromGlobals();
+        $node = $request->request->get('node');
+        $parent = $request->request->get('parent');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $object = $em->getRepository('AppBundle:Objects_tree')->find($node);
+
+        if(!$object) return new Response("Pas d'objet ya un truc chelou");
+
+
+        $parent = $em->getRepository('AppBundle:Objects_tree')->find($parent);
+        if(!$parent) return new Response("Pas de parents truc chelou");
+
+
+
+        $object->setContainerIn($parent);
+
+        $em->persist($object);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response("c ok trop de la boulette");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * @Route("/node/add", name="node_add")
      * @Method("POST")
