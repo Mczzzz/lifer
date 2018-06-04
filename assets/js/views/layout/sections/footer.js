4bf8b68ec773177d175cb8f92c0fdd1e831813bf
g.bailly <g.bailly@immanens.com>
2018-06-04 23:20:51 +0200

    mise à jour html

--- assets/js/views/layout/sections/footer.js
+++ assets/js/views/layout/sections/footer.js
@@ -11,7 +11,7 @@ export default class Footer{
 	return	`<footer class="page-footer gradient-45deg-purple-deep-orange">
 		      <div class="footer-copyright">
 		        <div class="container">
-		          <span></span>
+		          <input />
 		        </div>
 		      </div>
 		    </footer>`;
