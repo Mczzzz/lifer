54939df959fc9856e4197c51758567bb069b3da6
g.bailly <g.bailly@immanens.com>
2018-06-04 18:18:02 +0200

    mise à jour html

--- assets/js/views/components/header.js
+++ assets/js/views/components/header.js
@@ -9,17 +9,12 @@ export default class Header{
 	
 		console.log('header before super');
 		moment.locale('fr');
-     	let res = this.addTags();
-     	console.log(res);
-		return res;
 
 
 	}
 
 
 	addTags(){
-
-		console.log('on passe dans le haeder');
 
 	return	`<header id="header" class="page-topbar">';
 				<div class="navbar-fixed">
