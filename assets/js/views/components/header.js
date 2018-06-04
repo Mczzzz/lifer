fa5d4321fb54b2a7413b9368de7412256d8bd91c
g.bailly <g.bailly@immanens.com>
2018-06-04 18:19:07 +0200

    mise à jour html

--- assets/js/views/components/header.js
+++ assets/js/views/components/header.js
@@ -16,7 +16,7 @@ export default class Header{
 
 	addTags(){
 
-	return	`<header id="header" class="page-topbar">';
+	return	`<header id="header" class="page-topbar">
 				<div class="navbar-fixed">
 					<nav class="navbar-color gradient-45deg-purple-deep-orange gradient-shadow">
 						<div class="nav-wrapper">
