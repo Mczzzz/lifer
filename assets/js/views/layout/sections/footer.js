0c8603363a290e152330de178e32c0be2766dcca
g.bailly <g.bailly@immanens.com>
2018-06-04 23:32:03 +0200

    mise à jour html

--- assets/js/views/layout/sections/footer.js
+++ assets/js/views/layout/sections/footer.js
@@ -11,8 +11,10 @@ export default class Footer{
 	return	`<footer class="page-footer footer-fixed gradient-45deg-purple-deep-orange">
 		      <div class="footer-copyright">
 		        <div class="container">
-		          <input id="input_search"/>
-		          <label for="input_search">Recherche</label>
+			        <div class="input-field col s6">
+			          <input id="input_search"/>
+			          <label for="input_search">Recherche</label>
+			        </div>
 		        </div>
 		      </div>
 		    </footer>`;
