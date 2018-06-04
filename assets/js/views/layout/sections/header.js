186353eeb08d5b2982980418f6e1b51b30634374
g.bailly <g.bailly@immanens.com>
2018-06-04 20:20:20 +0200

    mise à jour html

--- assets/js/views/layout/sections/header.js
+++ assets/js/views/layout/sections/header.js
@@ -3,6 +3,15 @@ export default class Header{
 
 	constructor(){
 	
+
+		return	`<header id="header" class="page-topbar">
+				<div class="navbar-fixed">
+					<nav class="navbar-color gradient-45deg-purple-deep-orange gradient-shadow">
+						<div class="nav-wrapper">
+						</div>
+	        		</nav>
+	      		</div>
+	    	</header>`;
 	
 
 	}
