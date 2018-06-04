2afdaea69609bc26149b20e5843cf63effd7733e
g.bailly <g.bailly@immanens.com>
2018-06-04 22:08:34 +0200

    mise à jour html

--- assets/js/services/frame.js
+++ assets/js/services/frame.js
@@ -5,8 +5,9 @@ export default class Frame {
 
 	constructor(){
 
-	let homeFrame = new Home();
-	document.body.innerHTML = homeFrame;
+	let home = new Home();
+	let res = home.getHTMLPage();
+	document.body.innerHTML = res;
 
 	}
 
