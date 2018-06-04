3e12be40d745764b94c8a2bce3286ad0bd3a92c3
g.bailly <g.bailly@immanens.com>
2018-06-04 20:19:16 +0200

    mise à jour html

--- assets/js/services/frame.js
+++ assets/js/services/frame.js
@@ -5,63 +5,12 @@ export default class Frame {
 
 	constructor(){
 
-
-/*		let str = '<p>Just some <span>text</span> here</p>';
-
-		let temp = document.createElement('div');
-
-		temp.innerHTML()*/
-
-		//document.body.innerHTML = 'str';  // ex, add display objects 
-
-
-	/*	this.render();
-		
-		window.addEventListener('changeFrame', (e) => this[e.detail.frame](e));
-*/
-	let homeFrame = new Home(0,0,window.innerWidth,window.innerHeight);
-
-
-	}
-
-
-	render(){
-
-		let homeFrame = new Home(0,0,window.innerWidth,window.innerHeight);
-		let TheHomeFrame = homeFrame.load();
-		this.attach(TheHomeFrame);
+	let homeFrame = new Home();
 
 	}
 
 
 
-	node(){
-
-		console.log('in frame node');
-	
-		let nodeFrame = new Node(0,0,window.innerWidth,window.innerHeight);
-		let TheNode = nodeFrame.load();
-		this.attach(TheNode);
-
-	}
-
-
-	link(){
-
-		console.log('in frame link');
-	
-		let linkFrame = new Link(0,0,window.innerWidth,window.innerHeight);
-		let MyLinks = linkFrame.load();
-		this.attach(MyLinks);
-
-	}
-
-
-	attach(layout){
-
-		this.app.stage.addChild(layout);
-
-	}
 
 
 
--- assets/js/views/layout/home.js
+++ assets/js/views/layout/home.js
@@ -1,12 +1,9 @@
-import Header from '../components/header.js';
-import Footer from '../components/footer.js';
-import NodeList from '../components/NodeList.js';
-//import Adder from '../components/adder.js';
+import Header from './sections/header.js';
 
 export default class Home{
 
 
-	constructor(x,y,w,h){
+	constructor(){
 
 		
 
@@ -20,36 +17,20 @@ export default class Home{
 
 	addElements(){
 
-				let header = new Header(0,0,100,100);
+				let header = new Header();
 
+				console.log(header);
+				document.body.innerHTML = header;
+/*				
 				let toto = header.addTags();
 				document.body.innerHTML = toto;
-
-
-		
-
-/*
-		let footer = new Footer(0,this.height - 100,this.width,100);
-		let TheFooter = footer.load();
-		this.attach(TheFooter);
 */
-/*		const rayonButton = 30;
-		this.adder = new Adder(rayonButton);
-		this.adder.x = 20;
-		this.adder.y = window.innerHeight - (rayonButton * 2 ) - 20;
-		this.attach(this.adder);*/
+
+		
 
 
 	}
 		
-
-
-
-	drawBkground(){
-
-
-
-	}
 
 
 
