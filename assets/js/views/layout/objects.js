266aba1441d5ad38368e6680391a7162788f9739
g.bailly <g.bailly@immanens.com>
2018-06-06 09:40:15 +0200

    tes sur la home du click

--- assets/js/views/layout/objects.js
+++ assets/js/views/layout/objects.js
@@ -455,11 +455,12 @@ export default class Objects{
          	$('#breadcrumb').empty();
          	 for (let k in data.node.parents){
 				//console.log($('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).text);
+				console.log()
 				if($('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).text !== undefined){
-					$('#breadcrumb').prepend('<a href="#!" id=bc_'+data.node.parents[k].id+' class="custom-breadcrumb-item">'+$('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).text+'</a>');
+					$('#breadcrumb').prepend('<a href="#!" id=bc_'+$('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).id+' class="custom-breadcrumb-item">'+$('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).text+'</a>');
 					$('#bc_'+data.node.parents[k].id).click(function(){
 						$('#jstree_demo_div').show();
-						$('#jstree_object_tree').jstree(true).open_node(data.node.parents[k].id);
+						$('#jstree_object_tree').jstree(true).open_node($('#jstree_demo_div').jstree(true).get_node(data.node.parents[k]).id);
 
 
 					});
