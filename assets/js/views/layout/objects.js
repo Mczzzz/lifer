e33b172b900cce686227bc2f38c4e56bdb30615f
g.bailly <g.bailly@immanens.com>
2018-06-08 12:48:00 +0200

    Test du move avec ce vakata qui listen sur document WTF ;)

--- assets/js/views/layout/objects.js
+++ assets/js/views/layout/objects.js
@@ -24,7 +24,7 @@ export default class Objects{
 
     this.linkVakataDomEvent();
 
-    this.addEventListener('ContainerSelect', (e) => this.containerSelected(e));
+    //this.addEventListener('ContainerSelect', (e) => this.containerSelected(e));
 
 	}
 
