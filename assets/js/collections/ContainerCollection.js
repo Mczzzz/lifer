762a2dbf772abef67ae2cad607b0c44afa2da463
g.bailly <g.bailly@immanens.com>
2018-06-08 10:36:03 +0200

    Mise a jour des boutons de la home

---
+++ assets/js/collections/ContainerCollection.js
@@ -0,0 +1,76 @@
+import SvcBackEndComm from '../services/BackEndComm.js';
+
+export default class ContainerCollection {
+
+
+	constructor(){
+
+		this.apiPrefixe = "/container";
+
+    this.SvcBackEndComm = new SvcBackEndComm();
+
+
+	}
+
+
+  getAll(){
+
+    let result = this.SvcBackEndComm.ajaxSend('GET','all');
+
+    return JSON.parse(result.responseText);
+
+  }
+
+
+	save(){
+
+
+		let formData = new FormData();
+
+        //formData.append('jpg'  ,this.image);
+        formData.append('text'  ,$('#textarea').val());
+
+        formData.append('id'  ,$('#textarea').attr('node_id'));
+        let AjaxSender = $.ajax({
+              type: 'POST',
+              url: this.api,
+              data: formData,
+              async: true,
+              cache: false,
+              contentType: false,
+              processData: false
+            }).done((e) => this.getAll());
+
+
+
+
+
+	}
+
+
+
+
+
+	getOneNode(id){
+
+		let AjaxGetter = $.ajax({
+              type: 'GET',
+              url: this.api + "/" + id,
+              async: true,
+              cache: false,
+              contentType: false,
+              processData: false
+         })
+			.done(function( data ) {
+//				console.log('AJAX GET DONE');
+				let ret = JSON.parse(data);
+				$('#textarea').val(ret.data.text);
+				$('#textarea').attr('node_id',ret.data.id);
+    		});
+
+
+
+	}
+
+
+}
