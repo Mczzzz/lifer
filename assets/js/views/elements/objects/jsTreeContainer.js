dfabf182dcb5518ba265702864e6343853c1be47
g.bailly <g.bailly@immanens.com>
2018-06-10 22:49:01 +0200

    test du theme proton pour js tree

--- assets/js/views/elements/objects/jsTreeContainer.js
+++ assets/js/views/elements/objects/jsTreeContainer.js
@@ -86,7 +86,7 @@ export default class jsTreeContainer {
 			                "separator_after": false,
 			                "label": "Remove",
 			                "action": function (obj) {
-			                	this.onDeleteJsTree(false,obj);
+			                	onDeleteJsTree(false,obj);
 			                	}
 			                }
 			            }
