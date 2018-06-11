c0a1b3b3ec2afe0bd366a7ef5a125b276e13f4ff
g.bailly <g.bailly@immanens.com>
2018-06-11 21:12:44 +0200

    Factorisation de la toolBar

---
+++ assets/js/collections/ObjectInfosCollection.js
@@ -0,0 +1,56 @@
+import SvcBackEndComm from '../services/BackEndComm.js';
+
+export default class ObjectInfosCollection {
+
+
+	constructor(){
+
+		this.apiPrefixe = "/object/infos";
+
+    this.SvcBackEndComm = new SvcBackEndComm();
+
+
+	}
+
+
+  getList(id){
+
+    let result = this.SvcBackEndComm.ajaxSend('GET',this.apiPrefixe + 'get/'+ id);
+
+    return result.responseText;
+
+  }
+
+
+
+  rename(data){
+
+    let result = this.SvcBackEndComm.ajaxSend('POST',this.apiPrefixe + 'rename',data);
+
+    return result.responseText;
+
+  }
+
+
+
+  delete(data){
+
+    let result = this.SvcBackEndComm.ajaxSend('POST',this.apiPrefixe + 'delete',data);
+
+    return result.responseText;
+
+  }
+
+
+  create(data){
+
+    let result = this.SvcBackEndComm.ajaxSend('POST',this.apiPrefixe + 'add',data);
+
+    return result.responseText;
+
+  }
+
+
+
+
+}
