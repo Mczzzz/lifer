fea29cfccf9842fb80d18ce6193cdce7158d89c1
g.bailly <g.bailly@immanens.com>
2018-06-11 15:29:05 +0200

    Factorisation de la toolBar

--- assets/js/views/elements/objects/jsTreeContainer.js
+++ assets/js/views/elements/objects/jsTreeContainer.js
@@ -1,4 +1,4 @@
-import swal from 'sweetalert2';
+import swal from 'sweetalert';
 
 export default class jsTreeContainer {
 
--- assets/js/views/elements/objects/toolBarObjects.js
+++ assets/js/views/elements/objects/toolBarObjects.js
@@ -1,4 +1,4 @@
-import swal from 'sweetalert2';
+import swal from 'sweetalert';
 
 import toolBar from '../common/toolBar.js';
 
