5e138c1bf03664608c1f2b26e738da471260c1b3
g.bailly <g.bailly@immanens.com>
2018-06-13 17:28:25 +0200

    integration swipeable tabs jquery :(

--- assets/js/views/elements/objects/leafLoaderObjects.js
+++ assets/js/views/elements/objects/leafLoaderObjects.js
@@ -39,12 +39,12 @@ export default class leafLoaderObjects{
 
 	activeJQUERY(){
 
-			console.log($('#tabs-swipe-demo'));
+/*			console.log($('#tabs-swipe-demo'));
 		   $('#tabs-swipe-demo').tabs({
             swipeable : true,
             responsiveThreshold : 1920
           });
-
+*/
 
 	}
 
