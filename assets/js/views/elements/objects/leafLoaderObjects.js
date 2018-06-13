28078153608b364be041a5953ebd513e1c1c23e5
g.bailly <g.bailly@immanens.com>
2018-06-13 16:54:29 +0200

    ajout du retour sur les headers de page enfant

---
+++ assets/js/views/elements/objects/leafLoaderObjects.js
@@ -0,0 +1,48 @@
+export default class leafLoaderObjects{
+
+
+	constructor(){
+
+
+		this.addHTML();
+		this.activeJQUERY();
+	}
+
+
+	addHTML(){
+
+		let html = `<div id="Swipeable-tab" class="section">
+                              <h4 class="header">Swipeable Tabs</h4>
+                              <div class="row">
+                                <div class="col s12">
+                                  <ul id="tabs-swipe-demo" class="tabs">
+                                    <li class="tab col s3"><a href="#test-swipe-1">Test 1</a></li>
+                                    <li class="tab col s3"><a class="active" href="#test-swipe-2">Test 2</a></li>
+                                    <li class="tab col s3"><a href="#test-swipe-3">Test 3</a></li>
+                                  </ul>
+                                  <div id="test-swipe-1" class="col s12 carousel carousel-item blue white-text">
+                                    <div class="col s12 mt-1"></div>Test 1</div>
+                                </div>
+                                <div id="test-swipe-2" class="col s12 carousel carousel-item red white-text">
+                                  <div class="col s12 mt-1"></div>Test 2</div>
+                              </div>
+                              <div id="test-swipe-3" class="col s12 carousel carousel-item green white-text">
+                                <div class="col s12 mt-1"></div>Test 3</div>
+                            </div>`;
+                            
+        $('#leafInfos').append(html);
+
+	}
+
+
+	activeJQUERY(){
+
+		   $('ul.tabs').tabs({
+            swipeable : true,
+            responsiveThreshold : 1920
+          });
+
+
+	}
+
+}
