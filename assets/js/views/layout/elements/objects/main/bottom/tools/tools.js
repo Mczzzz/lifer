b78e46ecb2c0a5d43fbea04d15522dda0a4b936e
g.bailly <g.bailly@immanens.com>
2018-06-17 19:22:20 +0200

    relink header via layout

--- assets/js/views/layout/elements/objects/main/bottom/tools/tools.js
+++ assets/js/views/layout/elements/objects/main/bottom/tools/tools.js
@@ -69,7 +69,7 @@ export default class tools {
 	objects(){
 
 
-		let text = 'add';
+		let text = 'widgets';
 
 		let objects = document.createElement("a");
 		
@@ -86,7 +86,7 @@ export default class tools {
 
 		//css
 		this.toolsObjects.style.margin         = '17px';
-		this.toolsObjects.style.background     = 'linear-gradient(45deg, #0288d1 0%, #26c6da 100%)';
+		this.toolsObjects.style.background     = 'linear-gradient(45deg, #0097a7 0%, #b2ff59 100%)';
 		this.toolsObjects.style.width          = '46px';
 		this.toolsObjects.style.height         = '46px';
 		this.toolsObjects.style.borderRadius   = '50%';
