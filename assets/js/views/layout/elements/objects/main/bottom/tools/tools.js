6a1f5e20501b304bfe7ecddd6213de961e4ed18f
g.bailly <g.bailly@immanens.com>
2018-06-17 14:14:07 +0200

    relink header via layout

---
+++ assets/js/views/layout/elements/objects/main/bottom/tools/tools.js
@@ -0,0 +1,68 @@
+export default class tools {
+
+
+	constructor(){
+
+
+		this.initDivs();
+
+
+	}
+
+
+	initDivs(){
+
+
+
+	}
+
+
+/*
+floatingHTML(){
+
+		let floating =  `<div class="fixed-action-btn " style="bottom: 19px; left: 19px; width: 60px">
+		              <a class="btn-floating btn-large gradient-45deg-light-blue-cyan gradient-shadow">
+		                <i class="material-icons">add</i>
+		              </a>
+		              <ul>
+		                <li>
+		                  <a href=""  class="btn-floating grey">
+		                    <i class="material-icons">face</i>
+		                  </a>
+		                </li>
+		                <li>
+		                  <a href="#!" id="menu-obj"class="btn-floating green">
+		                    <i class="material-icons">widgets</i>
+		                  </a>
+		                </li>
+		                <li>
+		                  <a href="app-calendar.html" class="btn-floating grey">
+		                    <i class="material-icons">today</i>
+		                  </a>
+		                </li>
+		              </ul>
+		            </div>`;
+
+		$('body').append(floating);
+
+	}
+
+/*	footerHTML(){
+
+		new Footer();
+	}
+*/
+/*
+	activeJs(){
+
+
+	    $('#menu-obj').on('click', function() {
+
+	       let LinkEvent = new CustomEvent('changeFrame', {'detail' : {'frame' : 'Objects'}});
+	       window.dispatchEvent(LinkEvent);
+	    });
+
+
+	}*/
+
+}
