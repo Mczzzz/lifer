3f16e7db609dd93fa425eefb506a6392be76aab7
g.bailly <g.bailly@immanens.com>
2018-06-17 17:10:26 +0200

    relink header via layout

--- assets/js/views/layout/layout.js
+++ assets/js/views/layout/layout.js
@@ -62,7 +62,8 @@ export default class Layout {
 								}
 
 								.container{
-									
+									height: 100%;
+									width: 100%;
 									display: flex;
 									flex-direction: column;
 									justify-content: space-between;
