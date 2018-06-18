60fc500607ee88e91454e3c5529dbe4b530d9419
g.bailly <g.bailly@immanens.com>
2018-06-19 00:13:10 +0200

    raccord recherche sur page objects

--- assets/js/views/layout/layout.js
+++ assets/js/views/layout/layout.js
@@ -62,7 +62,7 @@ export default class Layout {
 								}
 
 								.container{
-									height: 100vh;
+									height: 100%;
 									width: 100%;
 									display: flex;
 									flex-direction: column;
