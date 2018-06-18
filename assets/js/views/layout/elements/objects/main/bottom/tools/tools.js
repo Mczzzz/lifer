9198ddc9387cd9c4e38482923ef3eb4edfd4fe2f
g.bailly <g.bailly@immanens.com>
2018-06-18 23:10:46 +0200

    raccord recherche sur page objects

--- assets/js/views/layout/elements/objects/main/bottom/tools/tools.js
+++ assets/js/views/layout/elements/objects/main/bottom/tools/tools.js
@@ -15,27 +15,23 @@ export default class tools {
 		this.toolsContainer.className = "toolsContainer";
 
 		this.toolsInput = document.createElement("input");
+		this.toolsInput.className = "toolsInput";
 		this.toolsInput.type = "text";
 		this.toolsInput.placeholder   = 'Recherche...';
 		this.toolsInput.style.display   = 'block';
 		this.toolsInput.style.padding   = '5px';
 		this.toolsInput.style.borderRadius   = '5px';
 		this.toolsInput.style.border   = '1px solid silver';
-		
 		this.toolsInput.style.height   = '2rem';
 
-	
-		this.toolsInput.className = "toolsInput";
 		this.toolsContainer.append(this.toolsInput);
-
-
 		$('.tools').append(this.toolsContainer);
 
 		let margeSize = 5;
-		console.log( $('.tools').width());
-		console.log( $('.tools').width() - margeSize * 2);
 		$('.toolsInput').css( "margin", margeSize );
 		$('.toolsInput').css( "width", $('.tools').width() - margeSize * 4);
+
+		$('.toolsContainer').css( "background", '#E8E8E8' );
 	}
 
  
