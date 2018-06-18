85eaac6fc439cf5ddfcbf07b2a3747c9a71098d2
g.bailly <g.bailly@immanens.com>
2018-06-19 01:00:02 +0200

    raccord recherche sur page objects

---
+++ assets/js/collections/LoaderCollection.js
@@ -0,0 +1,13 @@
+import Container from '../collections/ContainerCollection.js';
+import ObjectInfos from '../collections/ObjectInfosCollection.js';
+import ObjectInfosResourcesType from '../collections/ObjectInfosResourcesTypeCollection.js';
+import ObjectTree from '../collections/ObjectTreeCollection.js';
+
+export default class LoaderCollection {
+
+	constructor(collection){
+
+
+	}
+
+}
---
+++ assets/js/services/LoaderCollection.js
@@ -0,0 +1,14 @@
+import Container from '../collections/ContainerCollection.js';
+import ObjectInfos from '../collections/ObjectInfosCollection.js';
+import ObjectInfosResourcesType from '../collections/ObjectInfosResourcesTypeCollection.js';
+import ObjectTree from '../collections/ObjectTreeCollection.js';
+
+export default class LoaderCollection {
+
+	constructor(collection){
+
+		this.collection = new [collection]();
+
+	}
+
+}
