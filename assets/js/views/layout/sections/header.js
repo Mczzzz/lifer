e84496ac3f7d3d58f9df2e3cecaeb3627053d55b
g.bailly <g.bailly@immanens.coms>
2018-06-18 11:23:21 +0200

    raccord vers la page objects depuis la tool de la home

--- assets/js/views/layout/sections/header.js
+++ assets/js/views/layout/sections/header.js
@@ -4,9 +4,19 @@ export default class Header{
 	constructor(){
 	
 		this.backGroundColor =  '';
+		this.initContainer();
 
 	}
 
+	initContainer(){
+
+		let headerContainer = document.createElement("div");
+		headerContainer.className = "headercontainer";
+		$('.header').append(headerContainer);
+
+		$('.headerContainer ').css( "display", "flex" );
+		$('.headerContainer ').css( "justify-content", "space-between" );
+	}
 
 
 	setBkgdColor(color){
@@ -15,5 +25,10 @@ export default class Header{
 
 	}
 
+
+	setBackToHome(){
+
+		$(".nav-wrapper").append('<i id="BackToHome" class="material-icons" style="font-size:40px;margin-left:10px">keyboard_backspace</i>');
+	}
 
 }
