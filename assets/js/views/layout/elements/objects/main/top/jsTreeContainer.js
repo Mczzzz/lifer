e494f9b69e59b8a8f1842e34dce624ab78ff04dd
g.bailly <g.bailly@immanens.com>
2018-06-19 00:16:44 +0200

    raccord recherche sur page objects

--- assets/js/views/layout/elements/objects/main/top/jsTreeContainer.js
+++ assets/js/views/layout/elements/objects/main/top/jsTreeContainer.js
@@ -4,9 +4,9 @@ export default class jsTreeContainer {
 	constructor(HTMLParent){
 		console.log('in contructor');
 		//create the div to attach to parent
-		let JsTreeContainer = document.createElement("div");
-		JsTreeContainer.className = "JsTreeContainer";
-		$('.'+HTMLParent).append(JsTreeContainer);
+		let JsTreeContain = document.createElement("div");
+		JsTreeContain.className = "JsTreeContain";
+		$('.top').append(JsTreeContain);
 
 	}
 
