733efcdfb2f8dbc9c8e91496883ea271371885fa
g.bailly <g.bailly@immanens.com>
2018-06-24 00:22:03 +0200

    Mise à jour layout comportement de l'arbre parent

--- assets/js/views/layout/elements/objects/main/top/jsTreeContainer.js
+++ assets/js/views/layout/elements/objects/main/top/jsTreeContainer.js
@@ -248,7 +248,7 @@ export default class jsTreeContainer {
 
 	openNode(id){
 console.log(id);
-		let node = $('.'+this.MyClass).jstree(true).get_container();
+		let node = $('.'+this.MyClass).jstree(true).get_node(id);
 		console.log(node);
 		$('.'+this.MyClass).jstree(true).open_node(node);
 
