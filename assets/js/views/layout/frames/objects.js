cdf2979f475b60fa039c6c62e3d9a1e055db3a29
g.bailly <g.bailly@immanens.com>
2018-06-24 19:42:00 +0200

    fin d'ajustement des frames et voir si je peux intégrer le deuxieme breadcrumb

--- assets/js/views/layout/frames/objects.js
+++ assets/js/views/layout/frames/objects.js
@@ -114,6 +114,7 @@ export default class Objects extends layout{
       this.jsTreeParent.getHTMLParent()[0].style.flex = "1 1 0%";
       this.jsTreeChild.getHTMLParent()[0].style.flex = "";
       this.jsTreeChild.show();
+      this.mainBottomTools.clearSearch();
       this.in = "Container";
 
   }
@@ -152,7 +153,7 @@ export default class Objects extends layout{
 
 
       this.in = "Child";
-
+      this.mainBottomTools.clearSearch();
       
 
   }
