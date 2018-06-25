518f4ba89cb8cf06bea0fd6272762f22498ddda7
g.bailly <g.bailly@immanens.coms>
2018-06-25 18:20:01 +0200

    refacto Xieme fois des frame mais la jje croisa voir trouver le découpage correct :)

--- assets/js/views/elements/objects/main/main.js
+++ assets/js/views/elements/objects/main/main.js
@@ -1,12 +1,15 @@
-import MainBreadcrumb from './breadcrumb/breadcrumb.js';
-import MainTop from './top/top.js';
-import MainBottom from './bottom/bottom.js';
+import Breadcrumb from './breadcrumb/breadcrumb.js';
+import Top from './top/top.js';
+import Bottom from './bottom/bottom.js';
 
 export default class main {
 	
 
 	constructor(){
 
+		this.breadcrumb = new Breadcrumb();
+		this.top = new Top();
+		this.bottom = new Bottom();
 
 	}
 	
