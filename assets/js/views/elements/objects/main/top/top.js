07ec6be0e1fd9441cd3b1fcee4f60d673f188a65
g.bailly <g.bailly@immanens.com>
2018-06-26 02:14:51 +0200

    La facto commence à prendre :)

--- assets/js/views/elements/objects/main/top/top.js
+++ assets/js/views/elements/objects/main/top/top.js
@@ -55,7 +55,7 @@ export default class top {
 	callBack(data){
 		console.log("in top callback");
 		console.log(data);
-		let methode = "on_"+data.detail.detail.element+"_"+data.detail.detail.Event.type;
+		let methode = "on_"+data.detail.element+"_"+data.detail.detail.Event.type;
 		console.log(methode);
 		this[methode](data);
 
