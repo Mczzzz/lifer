a2db4d85efa6275d145143ac6ee4ff8568875503
g.bailly <g.bailly@immanens.coms>
2018-06-27 17:38:13 +0200

    ajout de la recherche en mode service

--- assets/js/services/search.js
+++ assets/js/services/search.js
@@ -12,12 +12,12 @@ export default class search {
 	addTarget(elMethode){
 
 		this.targets.push(elMethode);
-		console.log('addTarget');
-		console.log(this.targets);
+
 	}
 
+
 	addInput(el){
-		console.log('addInput');
+
 		this.inputs.push(el);	
 		el.element.addEventListener("keyup", (e)=> this.match(e,el.element));
 
@@ -39,25 +39,15 @@ export default class search {
 
 	match(e,el){
 
-		console.log('in match');
-		console.log(e);
-		console.log(el);
-		console.log(this.targets);
 
 		for(let target of this.targets){
-			console.log('in for');
-			console.log(target);
+
 			target.element[target.methode](el.value);
 
 		}
 
 	}
 
-
-
-	//les élements s'enregistre à moi
-
-	//je manipule ceux qui sont enregistrés
 
 
 }
