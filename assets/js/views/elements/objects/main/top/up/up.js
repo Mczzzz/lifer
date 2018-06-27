ad3cb341c4d73e854d85fe41485e29377d527aa0
g.bailly <g.bailly@immanens.coms>
2018-06-27 14:17:15 +0200

    ajout de la recherche en mode service

--- assets/js/views/elements/objects/main/top/up/up.js
+++ assets/js/views/elements/objects/main/top/up/up.js
@@ -24,7 +24,7 @@ export default class up {
 	initTree(){
 
 		this.jsTree = new jsTreeContainer(this.container,'Container','JsTreeContainer');
-		let res = this.jstree.getSearchElements();
+		let res = this.jsTree.getSearchElements();
 		console.log(res);
 		//this.searchService.addTarget();
     
