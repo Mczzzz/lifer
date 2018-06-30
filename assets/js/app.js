15430f0a39bd940d63d3062c6dcfb53f7c64b280
Guillaume BAILLY <g.bailly.akasha@gmail.com>
2018-06-30 23:47:19 +0200

    test service Lifer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 import { Lifer } from './services/Lifer.js';
 import Main from './controller/home.js';
 
+Lifer.addMe('','app');
+Lifer.dumpMe();
+
 const LiferApp = new Main('app');
 
-Lifer.addMe('','app');
