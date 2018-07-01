d02d61afb826d2d2bcd16cf1da093f654677ada7
g.bailly <g.bailly@immanens.com>
2018-07-01 22:02:57 +0200

    propagation Lifer

--- assets/js/services/Lifer.js
+++ assets/js/services/Lifer.js
@@ -80,7 +80,7 @@ addMe(path){
 		console.log(dataArray);
 
 		for(let myData of dataArray ){
-
+			console.log(myData);
 			console.log(Object.keys(myData));
 			dataRepresentation._data[Object.keys(myData)[0]] = myData[Object.keys(myData)[0]];
 
