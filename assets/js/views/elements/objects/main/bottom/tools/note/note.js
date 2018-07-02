e67ca92822844e8418582ba92b7879bcb8e5a3a7
g.bailly <g.bailly@immanens.coms>
2018-07-02 23:18:51 +0200

    ajout de la note

---
+++ assets/js/views/elements/objects/main/bottom/tools/note/note.js
@@ -0,0 +1,53 @@
+import superViews from "../../../../../../elements/common/super/views.js"
+
+export default class note extends superViews{
+
+
+	constructor(parent, MyClass,path){
+
+
+		super(parent,MyClass,path);
+
+		this.init();
+
+	}
+
+
+	init(){
+
+		this.toolbar();
+
+	}
+
+
+	toolbar(){
+
+		this.tb = document.createElement("div");
+		this.tb.style.height = "40px";
+		this.tb.style.background = "#e8eff7";
+		this.tb.style.display = "flex";
+	    this.container.append(div);
+
+	    this.addButton();
+	}
+
+
+	addButton(){
+
+		let button = document.createElement("div");
+		this.tb.append(button);
+
+		let i = document.createElement("i");
+			i.className = "material-icons";
+			i.style.fontSize = "25px";
+			i.style.color = "white";
+			i.style.marginRight = "15px";
+
+		i.append("note_add");
+
+		button.append(i);
+
+	}
+
+
+}
