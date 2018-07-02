f83246b63e114163a3ef1feca374c3c0a062a5c9
g.bailly <g.bailly@immanens.coms>
2018-07-02 16:39:43 +0200

    Mise ajour de lifer leaf

--- assets/js/views/elements/objects/main/bottom/manage/list/list.js
+++ assets/js/views/elements/objects/main/bottom/manage/list/list.js
@@ -31,6 +31,7 @@ export default class list extends superViews{
 	showList(){
 		console.log('in show list');
 		this.container.style.maxHeight = "300px";
+		this.container.overflowY = "scroll";
 		
 	}
 
