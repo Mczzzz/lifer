64b07274eda35d3f6636a3e43affa8f3cc2be30b
g.bailly <g.bailly@immanens.coms>
2018-07-02 13:51:05 +0200

    Mise ajour de lifer leaf

--- assets/js/views/elements/objects/main/main.js
+++ assets/js/views/elements/objects/main/main.js
@@ -47,9 +47,9 @@ export default class main extends superViews{
 	on_top_topDown_select_node(data){
 		console.log(data);
 
-		let ContainerNode = this.Lifer.addData("app/home/frame/objects","ContainerNode");
+		let ContainerNode = this.Lifer.getData("app/home/frame/objects","ContainerNode");
 		console.log(ContainerNode);
-		
+
 		this.breadcrumb.loadData(data.breadcrumb, 'bcContainer');
 		this.breadcrumb.show();
 		this.bottom.openManage(ojectId,leafId);
