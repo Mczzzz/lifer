b8a17030de846f6228f840004594655cdd0b806c
g.bailly <g.bailly@immanens.com>
2018-07-03 00:17:18 +0200

    Mise a jour list Style

--- assets/js/views/elements/common/ui/Note.js
+++ assets/js/views/elements/common/ui/Note.js
@@ -27,10 +27,10 @@ export default class Note extends superViews{
 	background(){
 
 		this.container.style.position = "absolute";
-		this.container.style.top = "2.5%";
-		this.container.style.left = "2.5%";
-		this.container.style.height = "95%";
-		this.container.style.width = "95%";
+		this.container.style.top = "1.5%";
+		this.container.style.left = "1.5%";
+		this.container.style.height = "97%";
+		this.container.style.width = "97%";
 		this.container.style.display = "flex";
 		this.container.style.background = "blue";
 
