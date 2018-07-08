d8dc804e742bcfdb04dfb2787891ac291c311f6f
g.bailly <g.bailly@immanens.com>
2018-07-08 22:14:23 +0200

    ajout de la ressource texte, create, update

--- assets/js/services/LoaderCollection.js
+++ assets/js/services/LoaderCollection.js
@@ -1,6 +1,6 @@
 import Container from '../collections/ContainerCollection.js';
 import ObjectInfos from '../collections/ObjectInfosCollection.js';
-import ObjectInfosResourcesType from '../collections/ObjectInfosResourcesCollection.js';
+import ObjectInfosResources from '../collections/ObjectInfosResourcesCollection.js';
 import ObjectTree from '../collections/ObjectTreeCollection.js';
 
 const classes = {
