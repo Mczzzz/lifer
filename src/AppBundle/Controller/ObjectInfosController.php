c41221bdb62b3d5157510c0d617da5352dc081ad
g.bailly <g.bailly@immanens.coms>
2018-07-11 09:34:02 +0200

    upload de l'image côté SF

--- src/AppBundle/Controller/ObjectInfosController.php
+++ src/AppBundle/Controller/ObjectInfosController.php
@@ -290,10 +290,11 @@ class ObjectInfosController extends Controller
         $ResourceId = $request->request->get('resourceId');
         $TypeId = $request->request->get('typeId');
 
-        var_dump($request->files);
-        die();
+
         foreach($request->files as $uploadedFile) {
-            $name = 'uploaded-file-name.jpg';
+            $name = $uploadedFile->originalName;
+                    var_dump($name);
+        die();
             $file = $uploadedFile->move('/var/www/html/lifer_data/', $name);
         }
 
