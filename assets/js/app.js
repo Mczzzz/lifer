58a9732b95fb64ee490d6cb745d5fac15a4089a8
g.bailly <g.bailly@immanens.coms>
2018-07-12 17:36:48 +0200

    gestion exif rotate image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 import { Lifer } from './services/Lifer.js';
 import Main from './controller/home.js';
 
+document.body.requestFullscreen();
+
 Lifer.addMe('','app');
 
 const LiferApp = new Main('app');
+
 
