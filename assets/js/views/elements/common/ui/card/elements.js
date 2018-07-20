44f96109e84dc8fbd8fbbbae06a53887ba1b5a16
g.bailly <g.bailly@immanens.coms>
2018-07-20 23:36:54 +0200

    Structuration de la card

---
+++ assets/js/views/elements/common/ui/card/elements.js
@@ -0,0 +1,54 @@
+import superViews from "../../super/views.js"
+
+import Text       from "./elements/text.js"
+import Button     from "../button.js"
+
+export default class header extends superViews{ 
+     
+
+     constructor(parent, MyClass,path,prepend = false){
+
+          super(parent, MyClass , path);
+
+
+          this.init();
+
+     }
+
+
+     init(){
+
+
+          this.container.style.display = "flex";
+          this.container.style.justifyContent = "space-around";
+
+          
+
+     }
+
+
+     addText(text,size = false, color = false,margin = false,weight =false,justify = false,suffixe = false){
+
+     	let TheText = new Text(this.container,"noteElement" + suffixe, this.path);
+     	TheText.setText(text);
+     	if (size)   TheText.setSize(size);
+     	if (color)  TheText.setColor(color);
+     	if (margin) TheText.setMargin(margin);
+     	if (weight) TheText.setWeight(weight);
+     	if (justify) this.setJustify(justify);
+
+     }
+
+
+     addButton(){
+
+     	let button = new Button(this.container,"buttonValid",this.path);
+     }
+
+     setJustify(justify){
+
+     	this.container.style.justifyContent = justify;
+     
+     }
+
+}
