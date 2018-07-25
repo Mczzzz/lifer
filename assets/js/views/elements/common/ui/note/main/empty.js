b4fd6b610cb6c65328659b959fc1584745702754
g.bailly <g.bailly@immanens.coms>
2018-07-25 22:42:08 +0200

    Mise a jour du setter de cards

--- assets/js/views/elements/common/ui/note/main/empty.js
+++ assets/js/views/elements/common/ui/note/main/empty.js
@@ -26,12 +26,18 @@ export default class Empty extends superViews{
 		this.card.setProperty("borderWidth", "0px");
 		this.card.setProperty("borderRadius", "0px");
 		this.card.setProperty("margin", "0px");
+		this.card.setProperty("padding", "10px");
+		this.card.setProperty("boxShadow", "0px -2px 12px #BBB");
+		this.card.setProperty("background", "white");
+
+		
 		//text, size, color,margin,weight,justify, suffixe
 /*		let HeaderElement = this.card.setElement("header");
 		this.card.pushElement(HeaderElement,this.data.update,9,"grey","0px 5px 2px 0px","normal","flex-end","update");*/
 
 
 		let EmptyElement = this.card.setElement("Empty");
+		this.
 		this.card.pushText(EmptyElement,"",20,"black","0px 5px 5px 5px","normal","flex-start","mainNewInput",1);
 		this.card.pushButton(EmptyElement,25,"green","flex-end");
 		
