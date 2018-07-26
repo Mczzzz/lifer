2d9a24d7c34f25605cf489d0acfcd7a81c86d7b0
g.bailly <g.bailly@immanens.coms>
2018-07-26 10:52:18 +0200

    Mise a jour du setter de cards

--- assets/js/views/elements/common/ui/note/main/resources.js
+++ assets/js/views/elements/common/ui/note/main/resources.js
@@ -36,8 +36,14 @@ export default class Resources extends superViews{
 			let carWidth = card.getWidth();
 
 			let HeaderElement = card.setElement("header_"+Resource.id);
-			card.pushText(HeaderElement,Resource.update,9,"grey","0px 5px 2px 0px","normal","flex-end","update_"+Resource.id);
+			card.setStyleElement(HeaderElement,"justifyContent","flex-end");
 
+					card.pushText(HeaderElement,"update",this.data.update);
+
+					card.setStyleComponent(HeaderElement,"update","fontSize","9px");
+					card.setStyleComponent(HeaderElement,"update","color","grey");
+					card.setStyleComponent(HeaderElement,"update","margin","0px 5px 2px 0px");
+					card.setStyleComponent(HeaderElement,"update","fontWeight","normal");
 
 			let MainElement = card.setElement("main"+Resource.id);
 
@@ -45,27 +51,22 @@ export default class Resources extends superViews{
 
 					let path = "/object/infos/resources/"+Resource.objectId+"/"+Resource.objectTreeId+"/"+Resource.objectInfoId+"/"+Resource.id;
 
-	//					cardElement = this.photoElement(Ressource.text,Ressource.id,"15px","","black","",Ressource.update);
 					card.pushImage(MainElement,path,"main_"+Resource.id,8,carWidth);
 
 				}else{
 
+					card.setStyleElement(MainElement,"justifyContent","flex-start");
+
+					card.pushText(MainElement,"main_"+Resource.id, Resource.text);
+
+					card.setStyleComponent(MainElement,"title","fontSize","15px");
+					card.setStyleComponent(MainElement,"title","color","black");
+					card.setStyleComponent(MainElement,"title","margin","0px 5px 5px 5px");
+					card.setStyleComponent(MainElement,"title","fontWeight","normal");
+
 					card.pushText(MainElement,Resource.text,15,"black","0px 5px 5px 5px","normal","flex-start","main_"+Resource.id);
 
 				}
-
-
-/*			this.card = new Card(this.container,'NoteTitleCard', this.path);
-			this.card.setId(this.data.id);
-
-			//text, size, color,margin,weight,justify, suffixe
-			let HeaderElement = this.card.setElement("header");
-			this.card.pushElement(HeaderElement,this.data.update,9,"grey","0px 5px 2px 0px","normal","flex-end","update");
-
-
-			let MainElement   = this.card.setElement("main");
-			this.card.pushElement(MainElement,this.data.name,20,"black","0px 5px 5px 5px","bold","flex-start","main");*/
-
 
 
 
