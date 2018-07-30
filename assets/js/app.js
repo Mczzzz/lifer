b01a1b6b0e792c48e78748e07a2e819fc4964c16
g.bailly <g.bailly@immanens.coms>
2018-07-30 22:59:16 +0200

    Mise a jour du setter de cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ import Controller from './controller/Controller.js';
 
 let name = "app";
 Lifer.addMe('',name);
-Lifer.addData(name,[{"Level" : 0}]);
+//Lifer.addData(name,[{"Level" : 0}]);
 
 const LiferApp = new Controller(name);
 
