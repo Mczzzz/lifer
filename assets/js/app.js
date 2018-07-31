a152131eaf9dd3c0a2714ed5d44d474592d8b62f
g.bailly <g.bailly@immanens.com>
2018-07-31 20:13:09 +0200

    test lifer  add

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ import Controller from './controller/Controller.js';
 
 let name = "app";
 console.log("addme app");
-Lifer.addMe('',name);
+Lifer.addMe(name);
 console.log("adddata app level 0");
 Lifer.addData(name,[{"Level" : 0}]);
 
