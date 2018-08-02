5cf03cfdb300f7a333f06bfcc55f36ef0385883f
g.bailly <g.bailly@immanens.coms>
2018-08-02 15:07:17 +0200

    j'ai oublié des fichiers :(

--- assets/js/views/elements/objects/main/bottom/manage/list.js
+++ assets/js/views/elements/objects/main/bottom/manage/list.js
@@ -42,8 +42,8 @@ export default class list extends superViews{
 
 		this.container.innerHTML ="";
 
-		let ContainerNode = this.Lifer.getData("app/home/frame/objects","ContainerNode");
-		let LeafNode = this.Lifer.getData("app/home/frame/objects","LeafNode");
+		let ContainerNode = this.Lifer.getData("objects","ContainerNode");
+		let LeafNode = this.Lifer.getData("objects","LeafNode");
 		
 		let ListInfos = this.collection.getList(ContainerNode.id,LeafNode.id);
 
