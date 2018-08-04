4a64d9aba0ee1a69a14bc4d35c12964a8547e17b
g.bailly <g.bailly@immanens.com>
2018-08-04 05:24:58 +0200

    test compatibilité Chrome Android

--- assets/js/views/elements/note/main/title.js
+++ assets/js/views/elements/note/main/title.js
@@ -90,6 +90,7 @@ export default class Title extends superViews{
 			let toFocus = document.getElementsByClassName("noteEltTextmainNewInput");
 
 			toFocus[0].focus();
+			toFocus[0].innetHTML = "";
     	}
 
 
