cf5d60b750b8fe4d53f67208f30e82abaa2f8ba3
g.bailly <g.bailly@immanens.com>
2018-08-09 23:44:02 +0200

    rtetour du backend

--- assets/js/services/BackendComm.js
+++ assets/js/services/BackendComm.js
@@ -20,7 +20,7 @@ export default class BackendComm {
 			  response.statusText //=> String
 			  response.headers    //=> Headers
 			  response.url        //=> String
-			  console.log(response);
+			  console.log(response.json());
 			  return response.text();
 			}, function(error) {
 			  error.message //=> String
