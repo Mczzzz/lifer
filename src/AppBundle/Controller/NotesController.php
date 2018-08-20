789625c07872e5657bdd6bd98f43db69698f6d89
g.bailly <g.bailly@immanens.coms>
2018-08-20 15:38:27 +0200

    Ajout de fixtures

--- src/AppBundle/Controller/NotesController.php
+++ src/AppBundle/Controller/NotesController.php
@@ -130,9 +130,6 @@ class NotesController extends Controller
         }
 
 
-            var_dump($datas->Note->Title);
-            die();
-
             $Note->setName($datas->Note->Title);
 
             if(!$datas->note->Ts){
