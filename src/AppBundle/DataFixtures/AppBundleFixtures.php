5067bd75f83a55091d3670eb956e665a3e67a3ac
g.bailly <g.bailly@immanens.coms>
2018-08-20 15:11:56 +0200

    Ajout de fixtures

---
+++ src/AppBundle/DataFixtures/AppBundleFixtures.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\ResourcesTypes;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        //On initialise les datas de fixtures
+        $RTArray = array();
+        array_push($RTArray, array( "Name" : "text", "Picto" : "title", "Enable" : true, "Color" : "blue") );
+        array_push($RTArray, array( "Name" : "image", "Picto" : "photo", "Enable" : true, "Color" : "orange") );
+
+
+        foreach ($array as $value){
+
+            $RT = new ResourcesTypes();
+            $RT->setName($value->Name);
+            $RT->setPicto($value->Picto);
+            $RT->setEnable($value->Enable);
+            $RT->setColor($value->Color);
+
+            $manager->persist($RT);
+
+
+        }
+
+        //on passe les enregistrements
+
+
+
+
+
+
+
+
+        $manager->flush();
+    }
+
+}
