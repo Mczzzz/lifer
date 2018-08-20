353cd016da7d94e1d44ff58aa36740a55bc0a4ce
g.bailly <g.bailly@immanens.coms>
2018-08-20 15:14:20 +0200

    Ajout de fixtures

--- src/AppBundle/DataFixtures/AppBundleFixtures.php
+++ src/AppBundle/DataFixtures/AppBundleFixtures.php
@@ -6,7 +6,7 @@ use AppBundle\Entity\ResourcesTypes;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class AppBundleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
