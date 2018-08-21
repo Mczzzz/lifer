6d395c33a5cdc92421e6dfc1625fc4ad37e660e3
root <root@ip-172-31-39-117.eu-central-1.compute.internal>
2018-08-21 10:34:31 +0000

    ajout au kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
         ];
 
