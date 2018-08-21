0b98db235c926ff4343cfc2bfbef3c1ef19ec041
g.bailly <g.bailly@immanens.coms>
2018-08-21 13:41:46 +0200

    rollback complet pfff

--- src/AppBundle/Controller/NotesController.php
+++ src/AppBundle/Controller/NotesController.php
@@ -121,7 +121,7 @@ class NotesController extends Controller
                     $Note = new Notes();
                     $Note->setCreator($user);
                     $Note->setTmpId($datas->Note->guid);
-                    //$Note->setCreateBDD('CURRENT_TIMESTAMP');
+                    $Note->setCreateBDD('CURRENT_TIMESTAMP');
              }else{
 
                 $Note = $NoteList[0];
