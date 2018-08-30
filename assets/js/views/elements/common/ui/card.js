e6a58aac6a9d09b962dce9882e8f37164c40728d
g.bailly <g.bailly@immanens.com>
2018-08-30 23:38:57 +0200

    Mise a jour Lifer.getData -> getObjectThisFromPath

--- assets/js/views/elements/common/ui/card.js
+++ assets/js/views/elements/common/ui/card.js
@@ -40,11 +40,11 @@ export default class card extends superViews{
       console.log(this.getContainer());
       console.log(this.getContainer().previousElementSibling);
       console.log(this.getContainer().nextSibling);
-      console.log(this.getContainer().previousElementSibling.getBoundingClientRect().y);
-      if(e.changedTouches[0].clientY > this.getContainer().previousElementSibling.getBoundingClientRect().y){
-        this.getContainer().parentElement.insertBefore(this.getContainer(),this.getContainer().previousElementSibling);
+      //console.log(this.getContainer().previousElementSibling.getBoundingClientRect().y);
+      //if(e.changedTouches[0].clientY > this.getContainer().previousElementSibling.getBoundingClientRect().y){
+      //  this.getContainer().parentElement.insertBefore(this.getContainer(),this.getContainer().previousElementSibling);
 
-      }
+      //}
 
 
 
