b38a64507574e90452238d82a5a8c107451e4234
root <root@ip-172-31-39-117.eu-central-1.compute.internal>
2018-09-01 20:59:33 +0000

    Ajout de UnityTypeEntity

---
+++ src/AppBundle/Entity/UnityType.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UnityType
+ *
+ * @ORM\Table(name="unity_type")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UnityTypeRepository")
+ */
+class UnityType
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="symbol", type="string", length=255, nullable=true)
+     */
+    private $symbol;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return UnityType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set symbol
+     *
+     * @param string $symbol
+     *
+     * @return UnityType
+     */
+    public function setSymbol($symbol)
+    {
+        $this->symbol = $symbol;
+
+        return $this;
+    }
+
+    /**
+     * Get symbol
+     *
+     * @return string
+     */
+    public function getSymbol()
+    {
+        return $this->symbol;
+    }
+}
+
