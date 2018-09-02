0a76c282626c11aeced3aed967bc39ffdf723ca4
g.bailly <g.bailly@immanens.com>
2018-09-02 14:48:02 +0200

    Ajout du Unity Controller côté SF

---
+++ src/AppBundle/Controller/UnityController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+use AppBundle\Entity\UnityType;
+
+use AppBundle\Helper\timestamp;
+
+/**
+ * Unity controller.
+ *
+ * @Route("api_v1/unity")
+ */
+
+class UnityController extends Controller
+{
+
+    /**
+     * @Route("/get-all-types", name="api_unitys_get_all_types")
+     * @Method("GET")
+    */
+
+    public function getAllTypeAction(Request $request)
+    {
+
+        // j'enregistre en base ma note
+        $em = $this->getDoctrine()->getManager();
+
+        $UnitiesTypeList = $em->getRepository('AppBundle:UnityType')->findAll();
+
+         $res = new \stdClass();
+
+        if(!$UnitiesTypeList){
+
+           
+            $res->error = 1;
+            $res->msg = "Aucune Unité disponible";
+
+            return new response(json_encode($res));
+
+
+        }
+
+        $res->data = array();
+
+        foreach($UnitiesTypeList as $UnityType){
+
+        $unity = new \stdClass();
+
+        $unity->id   = $UnityType->getId();
+        $unity->name   = $UnityType->getName();
+        $unity->symbol = $UnityType->getSymbol();
+
+        array_push($res->data,$unity);
+
+
+        }
+
+        $res->error = 0;
+        $res->msg = "OK";
+
+        return new response(json_encode($res));
+
+
+    }
+
+
+
+
+
+}
+
+
