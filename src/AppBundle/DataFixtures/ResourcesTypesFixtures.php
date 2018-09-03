ebdb1a2320df17d79abedf56a17971a9ffda6450
g.bailly <g.bailly@immanens.coms>
2018-09-03 10:16:39 +0200

    test de remontée callback

---
+++ src/AppBundle/DataFixtures/ResourcesTypesFixtures.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use AppBundle\Entity\ResourcesTypes;
+use AppBundle\Entity\UnityTypes;
+
+
+class ResourcesTypesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $this->ResourcesTypes($manager);
+
+    }
+
+
+
+    private function ResourcesTypes($manager){
+
+
+            //On initialise les datas de fixtures
+        $RTArray = array();
+        array_push($RTArray, array( "Name" => "text", "Picto" => "title", "Enable" => true, "Color" => "blue") );
+        array_push($RTArray, array( "Name" => "image", "Picto" => "photo", "Enable" => true, "Color" => "orange") );
+
+
+        foreach ($RTArray as $value){
+
+            $RT = new ResourcesTypes();
+            $RT->setName($value['Name']);
+            $RT->setPicto($value['Picto']);
+            $RT->setEnable($value['Enable']);
+            $RT->setColor($value['Color']);
+
+            $manager->persist($RT);
+
+
+        }
+
+        $manager->flush();
+
+    }
+
+
+
+
+}
