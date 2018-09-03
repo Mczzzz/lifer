35d77130a6c6a8e27157b793fee2953a406a5c04
g.bailly <g.bailly@immanens.coms>
2018-09-03 17:00:00 +0200

    test de remontée callback

--- assets/js/views/elements/note/main/empty.js
+++ assets/js/views/elements/note/main/empty.js
@@ -1,6 +1,7 @@
 import superViews from "../../common/super/views.js";
 
-import Card from "../../common/ui/card.js";
+import Text   from "./empty/text.js";
+import Number from "./empty/number.js";
 
 
 export default class Empty extends superViews{
@@ -25,38 +26,8 @@ export default class Empty extends superViews{
 
 	text(){
 
-		this.card = new Card('NoteEmptyCardText', this.path);
-		this.card.setId(0);
-		this.card.setStyle("borderWidth", "0px");
-		this.card.setStyle("borderRadius", "0px");
-		this.card.setStyle("margin", "0px");
-		this.card.setStyle("padding", "10px");
-		this.card.setStyle("boxShadow", "0px -2px 12px #BBB");
-		this.card.setStyle("background", "white");
-
-
-		this.EmptyElement = this.card.setElement("Empty");
-		this.card.setStyleElement(this.EmptyElement,"justifyContent","flex-start");
-
-		let TheTextElt = this.card.push("Text", this.EmptyElement,"mainNewInput", "");
-
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","fontSize","18.5px");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","color","black","property");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","margin","0px 5px 5px 5px");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","fontWeight","normal");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","flex",1);
-
-		let TheSaveButton = this.card.push("Button", this.EmptyElement,"mainNewButton","arrow_forward");
-
-		this.card.setStyleComponent(this.EmptyElement,"mainNewButton","alignItems","flex-end");
-
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","fontSize","25px");
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","marginRight","0px");
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","color","green");
-
-		TheSaveButton.getContainer().addEventListener("click",()=>this.saveResource(TheTextElt));
-
-		this.active = this.card;
+		let text = new Text("EmptyTextForm", this.path);
+		this.active = text;
 
 	}
 
@@ -64,85 +35,8 @@ export default class Empty extends superViews{
 
 	number(){
 
-		this.card = new Card('NoteEmptyCardNumber', this.path);
-		this.card.setId(0);
-		this.card.setStyle("borderWidth", "0px");
-		this.card.setStyle("borderRadius", "0px");
-		this.card.setStyle("margin", "0px");
-		this.card.setStyle("padding", "10px");
-		this.card.setStyle("boxShadow", "0px -2px 12px #BBB");
-		this.card.setStyle("background", "white");
-
-
-		this.EmptyElementLegend = this.card.setElement("NumberEmptyLegend");
-		this.card.setStyleElement(this.EmptyElementLegend,"justifyContent","flex-start");
-
-		let TheTextElt = this.card.push("Text", this.EmptyElementLegend,"mainNewInput", "");
-
-		this.card.setAttributeComponent(this.EmptyElementLegend,"mainNewInput","placeholder","Légende...");
-
-		this.card.setStyleComponent(this.EmptyElementLegend,"mainNewInput","fontSize","18.5px");
-		this.card.setStyleComponent(this.EmptyElementLegend,"mainNewInput","color","black","property");
-		this.card.setStyleComponent(this.EmptyElementLegend,"mainNewInput","margin","0px 5px 5px 5px");
-		this.card.setStyleComponent(this.EmptyElementLegend,"mainNewInput","fontWeight","normal");
-		this.card.setStyleComponent(this.EmptyElementLegend,"mainNewInput","flex",1);
-
-
-
-
-		this.EmptyElementValue = this.card.setElement("NumberEmptyValue");
-		this.card.setStyleElement(this.EmptyElementValue,"justifyContent","");
-
-		//////
-		let TheValueElt = this.card.push("Input", this.EmptyElementValue,"mainNewValue", "");
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewValue","flex",1);
-
-
-		this.card.setAttributeInputComponent(this.EmptyElementValue,"mainNewValue","placeholder","0000.000");
-		this.card.setAttributeInputComponent(this.EmptyElementValue,"mainNewValue","type","number");
-
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewValue","fontSize","18.5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewValue","color","black","property");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewValue","margin","0px 5px 5px 5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewValue","width","100%");
-		
-		///
-		let TheSelectElt = this.card.push("Select", this.EmptyElementValue,"mainNewSelect", this.Lifer.getData("Unity","Types"));
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewSelect","flex",1);
-
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","fontSize","18.5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","color","black","property");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","margin","0px 5px 5px 5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","fontWeight","normal");
-
-
-		let TheSelectUnity = this.card.push("Select", this.EmptyElementValue,"mainNewSelectUnity", "");
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewSelect","flex",1);
-
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelectUnity","fontSize","18.5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelectUnity","color","black","property");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelectUnity","margin","0px 5px 5px 5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelectUnity","fontWeight","normal");
-
-
-
-
-
-
-		let TheSaveButton = this.card.push("Button", this.EmptyElementValue,"mainNewButton","arrow_forward");
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewButton","alignItems","flex-end");
-
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","fontSize","25px");
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","marginRight","0px");
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","color","green");
-
-		TheSaveButton.getContainer().addEventListener("click",()=>this.saveResource(TheTextElt));
-
-		this.active = this.card;
+		let number = new Number("EmptyNumberForm", this.path);
+		this.active = number;
 
 	}
 
