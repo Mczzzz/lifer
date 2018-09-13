898316946a4fbbea8425f264b5f31e614bf7ec17
g.bailly <g.bailly@immanens.coms>
2018-09-13 17:48:14 +0200

    Mise  ajour du fichier manquant

--- assets/js/views/elements/note/main.js
+++ assets/js/views/elements/note/main.js
@@ -35,6 +35,10 @@ export default class Main extends superViews{
 
 	initChilds(){
 
+		let GUIDNote = this.Lifer.getData("Note/","This").note.guid;
+		console.log("GUIDNote");
+		console.log(GUIDNote);
+
 		this.Title     = new Title("noteMainTitle",this.path);
 
 		this.Resources = new Resources("noteMainResources",this.path);
@@ -42,84 +46,6 @@ export default class Main extends superViews{
 		this.Empty     = new Empty("noteMainEmpty",this.path);
 
 	}
-
-
-
-
-/*	_addRessources(){
-
-		let divCore = document.createElement("div");
-		divCore.style.overflowY = "scroll";
-		divCore.style.flex = "1";
-		this.Main.append(divCore);
-
-		this.divCore = divCore;
-
-		if(this.note.id){
-
-			console.log('in init noteAsked Id');
-			//load ma node pour récupérer le titre et la date
-			let res = this.NoteCollection.get(this.ContainerNode.id,this.LeafNode.id,this.note.id);
-
-			console.log(res);
-			console.log(this.Title);
-			if(res.name.length > 0){
-				let title = document.getElementById('title');
-				title.append(res.name);
-				title.style.color = "black";
-			}
-	
-			//je load mes ressources et je les affichent
-			let Ressources = this.ResourcesCollection.getList(this.ContainerNode.id, this.LeafNode.id, this.note.id);
-	
-			for (let Ressource of Ressources){
-
-					let cardElement = "";
-
-				if(Ressource.type == 3){
-
-					cardElement = this.photoElement(Ressource.text,Ressource.id,"15px","","black","",Ressource.update);
-
-				}else{
-
-					cardElement = this.textElement(Ressource.text,Ressource.id,"15px","","black","",Ressource.update);
-				}
-
-				divCore.append(cardElement);
-
-			}
-
-			console.log(divCore.scrollTop);
-			console.log(divCore.scrollHeight);
-			divCore.scrollTop = divCore.scrollHeight;
-
-		}
-
-
-
-	}*/
-
-
-
-
-
-
-/*	_addEmpty(){
-		let divEmpty = document.createElement("div");
-		divEmpty.style.display = "flex";
-		let cardEmpty = this.textElement('','text',"15px","","grey","Texte...");
-		divEmpty.append(cardEmpty);
-
-		cardEmpty.style.flex  = "1";
-		//let validEmpty = document.createElement("div");
-		
-		
-		let  button = new Button(divEmpty,"buttonValid",this.path);
-		//divEmpty.append(button);
-		this.Main.append(divEmpty);
-	
-	}*/
-
 
 
 
