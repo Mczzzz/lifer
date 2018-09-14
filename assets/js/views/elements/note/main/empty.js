43672273adf5a70d5408fed1a1b8dd72ac7f19b3
Guillaume Bailly <g.bailly@immanens.com>
2018-09-15 00:21:59 +0200

    Test animation

--- assets/js/views/elements/note/main/empty.js
+++ assets/js/views/elements/note/main/empty.js
@@ -58,7 +58,7 @@ export default class Empty extends superViews{
 
     	TitleCard.setStyle("position","absolute");
 		TitleCard.setStyle("width", "100%");
-		TitleCard.setStyle("top", top+"px");
+		TitleCard.setStyle("top", -20+top+"px");
 		TitleCard.setStyle("left", "0px");
 		
 
