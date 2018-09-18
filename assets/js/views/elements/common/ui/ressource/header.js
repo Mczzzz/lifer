887e5c8540d516526c2c127a69179314185c78e2
Guillaume Bailly <g.bailly@immanens.com>
2018-09-18 06:07:16 +0200

    Ajout du titre à la ressource

--- assets/js/views/elements/common/ui/ressource/header.js
+++ assets/js/views/elements/common/ui/ressource/header.js
@@ -25,19 +25,20 @@ export default class HeaderRessource extends superViews{
 		this.card.setStyle("borderRadius", "0px");
 		this.card.setStyle("margin", "0px");
 		this.card.setStyle("padding", "10px");
-		this.card.setStyle("background", "red");
+		this.card.setStyle("background", "white");
+		this.card.setStyle("border", "1px solid blue");
 
 
 
 
 		let HeaderElement = this.card.setElement("RessourceHeaderCardElement");
-		this.card.setStyleElement(HeaderElement,"justifyContent","flex-end");
+		this.card.setStyleElement(HeaderElement,"justifyContent","flex-start");
 
 				this.card.push("Text",HeaderElement,"titleRessource","");
 
 				this.card.setAttributeComponent(HeaderElement,"titleRessource","placeholder","Titre Ressource...");
 
-				this.card.setStyleComponent(HeaderElement,"titleRessource","9px");
+				this.card.setStyleComponent(HeaderElement,"titleRessource","11px");
 				this.card.setStyleComponent(HeaderElement,"titleRessource","color","grey");
 				this.card.setStyleComponent(HeaderElement,"titleRessource","margin","0px 5px 2px 0px");
 				this.card.setStyleComponent(HeaderElement,"titleRessource","fontWeight","normal");
