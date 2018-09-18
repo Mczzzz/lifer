4558cd0760b36719d20ae980fd7be1520758a4f6
Guillaume Bailly <g.bailly@immanens.com>
2018-09-18 07:06:11 +0200

    Ajout du titre à la ressource

--- assets/js/views/elements/common/ui/ressource/main/text.js
+++ assets/js/views/elements/common/ui/ressource/main/text.js
@@ -23,7 +23,7 @@ export default class Text extends superViews{
 
 	form(){
 
-		this.card = new Card('NoteEmptyCardText', this.path);
+		this.card = new Card('NoteEmptyCardText_'+this.ClassId, this.path);
 		this.card.setId(0);
 		this.card.setStyle("borderWidth", "0px");
 		this.card.setStyle("borderRadius", "0px");
@@ -33,24 +33,24 @@ export default class Text extends superViews{
 		this.card.setStyle("background", "white");
 
 
-		this.EmptyElement = this.card.setElement("Empty");
+		this.EmptyElement = this.card.setElement("EmptyText_"+this.ClassId);
 		this.card.setStyleElement(this.EmptyElement,"justifyContent","flex-start");
 
-		let TheTextElt = this.card.push("Text", this.EmptyElement,"mainNewInput", "");
+		let TheTextElt = this.card.push("Text", this.EmptyElement,"mainNewInput_"+this.ClassId, "");
 
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","fontSize","18.5px");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","color","black","property");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","margin","0px 5px 5px 5px");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","fontWeight","normal");
-		this.card.setStyleComponent(this.EmptyElement,"mainNewInput","flex",1);
+		this.card.setStyleComponent(this.EmptyElement,"mainNewInput_"+this.ClassId,"fontSize","18.5px");
+		this.card.setStyleComponent(this.EmptyElement,"mainNewInput_"+this.ClassId,"color","black","property");
+		this.card.setStyleComponent(this.EmptyElement,"mainNewInput_"+this.ClassId,"margin","0px 5px 5px 5px");
+		this.card.setStyleComponent(this.EmptyElement,"mainNewInput_"+this.ClassId,"fontWeight","normal");
+		this.card.setStyleComponent(this.EmptyElement,"mainNewInput_"+this.ClassId,"flex",1);
 
-		let TheSaveButton = this.card.push("Button", this.EmptyElement,"mainNewButton","arrow_forward");
+		let TheSaveButton = this.card.push("Button", this.EmptyElement,"mainNewButton_"+this.ClassId,"arrow_forward");
 
-		this.card.setStyleComponent(this.EmptyElement,"mainNewButton","alignItems","flex-end");
+		this.card.setStyleComponent(this.EmptyElement,"mainNewButton_"+this.ClassId,"alignItems","flex-end");
 
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","fontSize","25px");
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","marginRight","0px");
-		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton","color","green");
+		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton_"+this.ClassId,"fontSize","25px");
+		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton_"+this.ClassId,"marginRight","0px");
+		this.card.setStylePictoComponent(this.EmptyElement,"mainNewButton_"+this.ClassId,"color","green");
 
 		TheSaveButton.getContainer().addEventListener("click",()=>this.parentThis.saveResource(this.card,this.EmptyElement,TheTextElt));
 
