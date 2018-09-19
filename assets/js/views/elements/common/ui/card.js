3d930ad5568affcc1432fed0b1334f1a108607e9
g.bailly <g.bailly@immanens.coms>
2018-09-19 10:50:44 +0200

    Mise a jour du nommage des class d'objet

--- assets/js/views/elements/common/ui/card.js
+++ assets/js/views/elements/common/ui/card.js
@@ -147,7 +147,7 @@ export default class card extends superViews{
 
           this.Cloned.style.position = "absolute";
           this.Cloned.style.width = "100%";
-          this.Cloned.style.top = e.changedTouches[0].clientY;
+          this.Cloned.style.top = e.changedTouches[0].clientY+"px";
           console.log("top cloned");
           console.log(this.Cloned.style.top);
 
