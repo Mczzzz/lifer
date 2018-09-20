1f613a22768f7f98bdf2c2af00506ea211b7e979
g.bailly <g.bailly@immanens.coms>
2018-09-20 13:50:11 +0200

    réglage de la taille du titre de la note

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -103,12 +103,67 @@ export default class MainRessource extends superViews{
 		  console.log(this.Cloned.style.top);
 
 
-		}else if(type == "stop"){
+		}else if(type == "move"){
+
+        event.preventDefault();
+        //this.setStyle("background" , "red","element");
+
+        let MiddleCard = 0.5 * this.getContainer().getBoundingClientRect().height;
+
+
+        this.Cloned.style.top = e.changedTouches[0].clientY-MiddleCard+"px";
+        console.log(this.Cloned.style.top);
+        console.log("previous / next");
+        console.log(this.getContainer().previousElementSibling);
+        console.log(this.getContainer().nextElementSibling);
+        if(!this.getContainer().previousElementSibling && ((e.changedTouches[0].clientY - MiddleCard )< this.getContainer().getBoundingClientRect().y)){
+
+          this.Cloned.style.display = "none";
+
+        }else if(!this.getContainer().nextElementSibling && (e.changedTouches[0].clientY > this.getContainer().getBoundingClientRect().y)){
+
+          this.Cloned.style.display = "none";
+
+        }else{
+          this.Cloned.style.display = "";
+        }
+
+
+        if(this.getContainer().previousElementSibling){
+/*          console.log("touchY:"+e.changedTouches[0].clientY);
+          console.log("contHeight:"+ this.getContainer().getBoundingClientRect().height);
+          console.log( JSON.stringify(this.getContainer().getBoundingClientRect()));
+          console.log( JSON.stringify(this.getContainer().previousElementSibling.getBoundingClientRect()));
+          console.log("previousPosY"+this.getContainer().previousElementSibling.getBoundingClientRect().y);
+          console.log("----------------------------------------------");*/
+          
+          if((e.changedTouches[0].clientY - this.getContainer().getBoundingClientRect().height ) < this.getContainer().previousElementSibling.getBoundingClientRect().y){
+
+            this.getContainer().parentElement.insertBefore(this.getContainer(),this.getContainer().previousElementSibling);
+          
+          }
+
+        }
+
+        if(this.getContainer().nextElementSibling){
+
+
+          if((e.changedTouches[0].clientY ) > this.getContainer().nextElementSibling.getBoundingClientRect().y){
+
+            this.getContainer().parentElement.insertBefore(this.getContainer(),this.getContainer().nextElementSibling.nextElementSibling);
+          
+          }
+
+        }
+    
+
+
+      }else if(type == "stop"){
 
         //this.setStyle("background" , "white","element");
-        	this.Cloned.remove();
+        this.Cloned.remove();
 
-      	}
+      }
 
 
 
