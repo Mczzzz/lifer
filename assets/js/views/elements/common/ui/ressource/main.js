59ddc1fc62286aceb3ea3f132b0b127002ff46a0
Guillaume Bailly <g.bailly@immanens.com>
2018-09-21 23:57:04 +0200

    Finalisation bug draggable list

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -239,7 +239,7 @@ export default class MainRessource extends superViews{
 
 	        }
 
-	        let nodeTotest = childContainer.getContainer();
+/*	        let nodeTotest = childContainer.getContainer();
 	        while( parseInt(nodeTotest.style.marginLeft, 10) >= GoodMargin){
 
 	        	console.log('in while');
@@ -247,7 +247,7 @@ export default class MainRessource extends superViews{
 
 
 	        	nodeTotest = nodeTotest.nextElementSibling;
-	        }
+	        }*/
 
 
 
