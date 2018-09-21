75f4e0b3f057dfc3a81aa3024f2fec021518e0b9
g.bailly <g.bailly@immanens.coms>
2018-09-21 15:10:02 +0200

    Mise a jour du nommage des class d'objet

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -141,7 +141,7 @@ export default class MainRessource extends superViews{
 
 	          this.Cloned.style.display = "none";
 
-	        }else if(!childContainer.getContainer().nextElementSibling && (e.changedTouches[0].clientY > childContainer.getBoundingClientRect().y)){
+	        }else if(!childContainer.getContainer().nextElementSibling && (e.changedTouches[0].clientY > (this.Ghost.getBoundingClientRect().y + this.Ghost.getBoundingClientRect().width ))){
 
 	          this.Cloned.style.display = "none";
 
