ae1c11a4ca9e3c82c8d8267ebaf75d65d6dd360a
g.bailly <g.bailly@immanens.coms>
2018-09-21 14:27:41 +0200

    Mise a jour du nommage des class d'objet

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -156,7 +156,7 @@ export default class MainRessource extends superViews{
 	        if(childContainer.getContainer().previousElementSibling.previousElementSibling){
 	        	console.log('in previous');
 	          
-	          if((e.changedTouches[0].clientY - this.Ghost.getBoundingClientRect().height ) < this.Ghost.previousElementSibling.previousElementSibling.getBoundingClientRect().y){
+	          if(e.changedTouches[0].clientY < this.Ghost.previousElementSibling.getBoundingClientRect().y){
 	          	console.log('in previous move node');
 	          	childContainer.getContainer().parentElement.insertBefore(childContainer.getContainer(),childContainer.getContainer().previousElementSibling.previousElementSibling);
 	            childContainer.getContainer().parentElement.insertBefore(this.Ghost,childContainer.getContainer());
