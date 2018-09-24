2910d9521b46ee83e8ec8e3f5317fbb552f6a10b
g.bailly <g.bailly@immanens.coms>
2018-09-24 12:52:47 +0200

    ui chemin user note

--- assets/js/views/elements/common/ui/ressource/header.js
+++ assets/js/views/elements/common/ui/ressource/header.js
@@ -121,7 +121,7 @@ export default class HeaderRessource extends superViews{
       if(this.firstClick == false){
         console.log(this.firstClick);
         this.menu.destroyMe();
-        document.removeEventListener("click", (e)=>this.closeDropDown(e),false);
+        document.removeEventListener("click", this.closeDropDown(),true);
         this.firstClick = true;
 
       }else{
