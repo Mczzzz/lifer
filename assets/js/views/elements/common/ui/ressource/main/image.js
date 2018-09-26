3e5073219a518a7f5421a87b5f81a1b9378c5ae9
g.bailly <g.bailly@immanens.coms>
2018-09-26 16:47:49 +0200

    ui chemin user note

--- assets/js/views/elements/common/ui/ressource/main/image.js
+++ assets/js/views/elements/common/ui/ressource/main/image.js
@@ -41,78 +41,18 @@ export default class Image extends superViews{
 		this.card.setStyleElement(this.EmptyElement,"justifyContent","flex-start");
 		this.card.setStyleElement(this.EmptyElement,"flexWrap","wrap");
 
+		this.camLauncher = document.createElement("input");
+		this.camLauncher.type = "file";
+		this.camLauncher.accept = type+"/*";
 
-		let TheTextElt = this.card.push("Text", this.EmptyElement,"Input_"+this.ClassId, "");
+		//if(capture){
+			this.camLauncher.capture = "camera";	
+		//}
 
-		this.card.setAttributeComponent(this.EmptyElement,"Input_"+this.ClassId,"placeholder","Légende...");
+		this.camLauncher.style.display = "none";
+		this.container.append(this.camLauncher);
+		this.camLauncher.click();
 
-		this.card.setStyleComponent(this.EmptyElement,"Input_"+this.ClassId,"fontSize","18.5px");
-		this.card.setStyleComponent(this.EmptyElement,"Input_"+this.ClassId,"color","black","property");
-		this.card.setStyleComponent(this.EmptyElement,"Input_"+this.ClassId,"margin","0px 5px 5px 5px");
-		this.card.setStyleComponent(this.EmptyElement,"Input_"+this.ClassId,"fontWeight","normal");
-		this.card.setStyleComponent(this.EmptyElement,"Input_"+this.ClassId,"flex","1 1 100%");
-
-
-
-		let TheValueElt = this.card.push("Input", this.EmptyElement,"Number_"+this.ClassId, "");
-
-		this.card.setStyleComponent(this.EmptyElement,"Number_"+this.ClassId,"flex","1 1 0%");
-
-
-		this.card.setAttributeInputComponent(this.EmptyElement,"Number_"+this.ClassId,"placeholder","0.0");
-		this.card.setAttributeInputComponent(this.EmptyElement,"Number_"+this.ClassId,"type","number");
-
-		this.card.setStyleInputComponent(this.EmptyElement,"Number_"+this.ClassId,"fontSize","18.5px");
-		this.card.setStyleInputComponent(this.EmptyElement,"Number_"+this.ClassId,"color","black","property");
-		this.card.setStyleInputComponent(this.EmptyElement,"Number_"+this.ClassId,"background","transparent","all");
-		this.card.setStyleInputComponent(this.EmptyElement,"Number_"+this.ClassId,"margin","0px 5px 5px 5px");
-		this.card.setStyleInputComponent(this.EmptyElement,"Number_"+this.ClassId,"width","100%");
-		
-
-		//on crée un bouton
-		let InutiesTool = this.card.push("Button", this.EmptyElement,"Unit_"+this.ClassId,{"picto" : "fas fa-weight-hanging", "fontType" : "fas"});
-
-		this.card.setStyleComponent(this.EmptyElement,"Unit_"+this.ClassId,"flex","1.2");
-		this.card.setStyleComponent(this.EmptyElement,"Unit_"+this.ClassId,"marginLeft","15px");
-
-		this.card.setStylePictoComponent(this.EmptyElement,"Unit_"+this.ClassId,"fontSize","25px");
-		this.card.setStylePictoComponent(this.EmptyElement,"Unit_"+this.ClassId,"marginRight","0px");
-		this.card.setStylePictoComponent(this.EmptyElement,"Unit_"+this.ClassId,"color","green");
-
-		InutiesTool.getContainer().addEventListener("click",()=>this.Startselect());
-		//////
-		
-
-
-
-
-/*
-		///
-		let TheSelectElt = this.card.push("Select", this.EmptyElementValue,"mainNewSelect", this.Lifer.getData("Unity","Types"));
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewSelect","flex",1);
-
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","fontSize","18.5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","color","black","property");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","margin","0px 5px 5px 5px");
-		this.card.setStyleInputComponent(this.EmptyElementValue,"mainNewSelect","fontWeight","normal");
-
-		TheSelectElt.addEventListener("change", (e))
-
-
-
-		let TheSaveButton = this.card.push("Button", this.EmptyElementValue,"mainNewButton","arrow_forward");
-
-		this.card.setStyleComponent(this.EmptyElementValue,"mainNewButton","alignItems","flex-end");
-
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","fontSize","25px");
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","marginRight","0px");
-		this.card.setStylePictoComponent(this.EmptyElementValue,"mainNewButton","color","green");
-
-		TheSaveButton.getContainer().addEventListener("click",()=>this.saveResource(TheTextElt));*/
-
-		/*this.active = this.card;
-*/
 	}
 
 
