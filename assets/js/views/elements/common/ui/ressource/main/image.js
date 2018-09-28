efd6fdbfe938a7cab4d0c4d97c42e4045df8a47d
g.bailly <g.bailly@immanens.coms>
2018-09-28 10:51:46 +0200

    ui chemin user note

--- assets/js/views/elements/common/ui/ressource/main/image.js
+++ assets/js/views/elements/common/ui/ressource/main/image.js
@@ -79,7 +79,7 @@ export default class Image extends superViews{
 		viewCard.setStyle("height", "100%");
 		viewCard.setStyle("background", "red");
 
-		PictElt = viewCard.setElement("PictElt");
+		let PictElt = viewCard.setElement("PictElt");
 		viewCard.push("Image", PictElt, "MyPict", data);
 
 
