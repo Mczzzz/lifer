e4055b58b1cdf07943de4c8843dd3aee3798fd89
Guillaume Bailly <g.bailly@immanens.com>
2018-09-29 08:36:57 +0200

    collapse for ever

--- assets/js/views/elements/common/ui/ressource/main/image.js
+++ assets/js/views/elements/common/ui/ressource/main/image.js
@@ -48,8 +48,8 @@ export default class Image extends superViews{
 
 		//this.camLauncher.style.display = "none";
 		this.container.append(this.camLauncher);
-		console.log("click sur image");
-
+		//console.log("click sur image");
+			
 		this.camLauncher.click();
 		this.camLauncher.addEventListener("change", ()=>LoaderImage.importPict(this.camLauncher.files[0],this.path));
 
