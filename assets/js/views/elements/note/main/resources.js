4dfbffab2472d6b655f9f738a49d60b887d8b869
Guillaume Bailly <g.bailly@immanens.com>
2018-10-13 16:09:01 +0200

    récupération de la liste

--- assets/js/views/elements/note/main/resources.js
+++ assets/js/views/elements/note/main/resources.js
@@ -106,6 +106,7 @@ export default class Resources extends superViews{
 		let ChildList = EditRessource.getContainer().childNodes;
 
 		console.log(ChildList);
+		console.log(ChildList.reverse());
 
 
 	}
