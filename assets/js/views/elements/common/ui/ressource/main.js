b7c3cdd1c86fd14b8c5cddfad41f1bb96f4f7226
g.bailly <g.bailly@immanens.coms>
2018-10-22 16:57:34 +0200

    Mise à jour de la taille de l'ecran

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -42,7 +42,7 @@ export default class MainRessource extends superViews{
 
 
      resize(){
-
+     	console.log('####### resize #######');
      	this.container.scrollTop = this.container.scrollHeight;
 
      }
