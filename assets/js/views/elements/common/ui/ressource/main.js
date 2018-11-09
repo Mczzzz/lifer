d3063aa952449f81b6ee0d215367f0a879028061
Guillaume Bailly <g.bailly@immanens.com>
2018-11-09 22:57:48 +0100

    on regle les marges

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -527,7 +527,7 @@ export default class MainRessource extends superViews{
 
 	        this.Cloned.style.marginLeft = "Opx";
 	        this.Cloned.style.top = e.changedTouches[0].clientY - this.touchY+"px";
-	        this.Cloned.style.left =  ( this.touchX - this.initMarginClone) +"px";
+	        this.Cloned.style.left =  ( this.touchX + this.initMarginClone) +"px";
 
 	        //on fait bouger les enfants aussi :) :
 	         for (let childMove of this.childTomove) {
