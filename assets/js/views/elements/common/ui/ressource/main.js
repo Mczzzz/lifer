bf6ba345394b48dcf174c6285f0ede8e89e7a62f
Guillaume Bailly <g.bailly@immanens.com>
2018-11-12 23:00:01 +0100

    MAJ taille du ghost

--- assets/js/views/elements/common/ui/ressource/main.js
+++ assets/js/views/elements/common/ui/ressource/main.js
@@ -426,14 +426,14 @@ export default class MainRessource extends superViews{
 		this.container.scrollTop = this.container.scrollTop - ratio;
 		if(this.container.scrollTop == 0){
 
-			clearInterval(this.movementId);
-			this.inMovement = false;
+			this.stopScroll();
 		} 
 	}
 
 	stopScroll(){
 
-
+		clearInterval(this.movementId);
+		this.inMovement = false;
 	}
 
 
