0e719748422f96256e0ea37ce5c8681b2a7444d6
g.bailly <g.bailly@immanens.coms>
2018-11-21 10:44:12 +0100

    Mise a jour margin

--- assets/js/services/DatasSynchronizing.js
+++ assets/js/services/DatasSynchronizing.js
@@ -22,7 +22,7 @@ class DatasSynchronizing {
 	init(){
 
 		this.syncData = openDatabase('syncData', '1.0', 'queue de synchronisation', 2 * 1024 * 1024);
-		this.playQuery('CREATE TABLE IF NOT EXISTS Commandes (id unique, note_id, note_title, ressource_id, ressource_title, item_id, item_type, item_text, item_value, item_path,item_unit)');
+		this.playQuery('CREATE TABLE IF NOT EXISTS Commandes (id PRIMARY KEY, note_id, note_title, ressource_id, ressource_title, item_id, item_type, item_text, item_value, item_path,item_unit)');
 
 
 	}
