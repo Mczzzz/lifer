4fa505ed6307e6b02401f255d6ecec24ed970a93
g.bailly <g.bailly@immanens.coms>
2018-12-03 18:14:05 +0100

    Maj by controller

---
+++ assets/js/controller/NoteController.js
@@ -0,0 +1,32 @@
+import Note from '../views/frames/note.js';
+
+export default class NoteController {
+
+
+	constructor(){
+
+
+		this.init();
+	}
+
+
+
+
+	init(){
+
+
+		this.initView();
+	}
+
+
+
+
+	initView(){
+
+
+		let note = new Note("Note",null);
+		
+	}
+
+
+}
