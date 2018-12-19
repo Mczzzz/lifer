1063b1c3556c85f9e9a0039644a8e0adb3065255
g.bailly <g.bailly@immanens.coms>
2018-12-19 12:05:42 +0100

    remonté de la data

--- assets/js/views/common/ui/card.js
+++ assets/js/views/common/ui/card.js
@@ -57,65 +57,6 @@ export default class card extends superViews{
      }
 
 
-//TO DESTROY
-/*     setStyleElement(element,property,value,scope = "all"){
-
-          //console.log(element);
-
-          this[element.MyClass].setStyle(property,value,scope);
-
-     }*/
-
-
-//TO DESTROY
-/*     setStyleComponent(element,component, property, value,scope = "all"){
-
-          this[element.MyClass].setStyleComponent(component, property, value,scope);
-
-     }*/
-
-
-
-//TO DESTROY
-/*     setStylePictoComponent(element,component, property, value,scope = "all"){
-
-          this[element.MyClass].setStylePictoComponent(component, property, value,scope);
-
-     }*/
-
-
-
-/*     setAttributeComponent(element,component, property, value,scope = "all"){
-
-        if(property == "draggable"){
-
-            value = true;
-            this[element.MyClass].initTouch(component,this.path,'touchMover');
-        
-        }
-
-          this[element.MyClass].setAttributeComponent(component, property, value,scope);
-
-     }*/
-
-
-////////////////
-//TO DESTROY
-/*     setStyleInputComponent(element,component, property, value,scope = "all"){
-
-          this[element.MyClass].setStyleInputComponent(component, property, value,scope);
-
-     }*/
-
-///////////////
-/*     setAttributeInputComponent(element,component, property, value,scope = "all"){
-
-
-          this[element.MyClass].setAttributeInputComponent(component, property, value,scope);
-
-     }*/
-
-
 
 
      push(type,element,classSuffixe,data,prepend = false){
@@ -169,7 +110,7 @@ export default class card extends superViews{
   //    console.log(this.callBack);
         for (let eventsToCallBack of this.callBack) {
 
-   //        console.log('in FOR');
+          console.log('Card Dispatch Event');
           console.log(eventsToCallBack);
            let objectToCallBack = this.getObjectThisfromPath(eventsToCallBack.path);
            //console.log();
