339f014094b80b15766b1eb87d81e27ffde72179
g.bailly <g.bailly@immanens.coms>
2018-12-21 12:44:05 +0100

    remonté de la data

--- assets/js/collections/NoteCollection.js
+++ assets/js/collections/NoteCollection.js
@@ -67,7 +67,7 @@ export default class NoteCollection {
 		let TblNote = {};
 		TblNote.name = "Notes";
 		TblNote.db = "syncData";
-		TblNote.create = `CREATE TABLE IF NOT EXISTS `+TblNote.name+` (timestamp DATETIME,
+		TblNote.create = `CREATE TABLE IF NOT EXISTS `+TblNote.name+` (timestamp FLOAT,
 																	   status,
 																	   note_id,
 																	   note_title,
