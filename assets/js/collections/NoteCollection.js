63cfd98d498919d694b58ab5d1b6db5349a15478
Guillaume Bailly <g.bailly@immanens.com>
2018-12-23 13:54:22 +0100

    Mise a jour reponse

--- assets/js/collections/NoteCollection.js
+++ assets/js/collections/NoteCollection.js
@@ -234,7 +234,7 @@ export default class NoteCollection {
 		//on regarde si on a du temporaire pour les id
 		let NoteId = (datas.data.Note.tmpId) ? datas.data.Note.tmpId : datas.data.Note.id;
 		let ResourceId = (datas.data.Resource.tmpId) ? datas.data.Resource.tmpId : datas.data.Resource.id;
-		let ItemId = (datas.data.Item.tmpId) ? datas.data.Note.Item.tmpId : datas.data.Item.id;
+		let ItemId = (datas.data.Item.tmpId) ? datas.data.Item.tmpId : datas.data.Item.id;
 
 		//je supprime la ligne de sync up
 		let qry = `DELETE FROM Notes 
