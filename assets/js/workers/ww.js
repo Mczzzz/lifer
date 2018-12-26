4ef6c61f1aa81c60ead8a4ac5be7a129048723f9
Guillaume Bailly <g.bailly@immanens.com>
2018-12-27 00:14:43 +0100

    Test persistant storage*

--- assets/js/workers/ww.js
+++ assets/js/workers/ww.js
@@ -2,6 +2,6 @@ self.addEventListener('message', function(e) {
 	console.log(e);
   self.postMessage(e.data);
 }, false);
-this.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
     // it can be empty if you just want to get rid of that error
 });
