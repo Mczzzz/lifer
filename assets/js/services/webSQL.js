bb36ea923590af5a5a3f4e53df8a4fe0b23f9f6e
Guillaume Bailly <g.bailly@immanens.com>
2018-12-28 15:37:08 +0100

    Nettoyage des logs

--- assets/js/services/webSQL.js
+++ assets/js/services/webSQL.js
@@ -23,16 +23,16 @@ export default class webSQL{
 	execQuery(db,query,args = false, callbackObj = false, callbackMethod = false){
 
 	if(!args) args = [];
-		console.log(query);
+	//	console.log(query);
 		db.executeSql(query,args,(tx,results)=>this.webSQLsucess(tx,results,callbackObj,callbackMethod),(tx,errors)=>this.webSQLerror(tx,errors));
 	}
 
 
 	webSQLsucess(tx,results,callbackObj = false, callbackMethod = false){
 
-		console.log("webSQLsuccess");
+/*		console.log("webSQLsuccess");
 		console.log(tx);
-		console.log(results);
+		console.log(results);*/
 		if(callbackObj && callbackMethod){
 			callbackObj[callbackMethod](results);
 		}
@@ -42,9 +42,9 @@ export default class webSQL{
 
 	webSQLerror(tx,errors){
 
-		console.log("webSQLerror");
+/*		console.log("webSQLerror");
 		console.log(tx);
-		console.log(errors);
+		console.log(errors);*/
 
 	}
 
