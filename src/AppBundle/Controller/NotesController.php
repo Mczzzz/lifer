ff313580cd7098356d208532809e11d376583499
Guillaume Bailly <g.bailly@immanens.com>
2018-12-30 12:57:25 +0100

    Mise a jour d'engregistrement

--- src/AppBundle/Controller/NotesController.php
+++ src/AppBundle/Controller/NotesController.php
@@ -362,6 +362,99 @@ class NotesController extends Controller
             return $Item;
     }
 
+
+
+
+ /**
+     * @Route("/pushDatas", name="api_notes_push_datas")
+     * @Method("POST")
+    */
+
+    public function pushDatasAction(Request $request)
+    {
+
+/*        Errors:
+        1 : Security
+        2 : Data Problem
+        3 : Crash*/
+
+        $res = new \stdClass();
+        $res->error = "";
+        $res->msg = "";
+
+
+/*        // is it an Ajax request?
+        $isAjax = $request->isXmlHttpRequest();
+ 
+        if(!$isAjax){
+
+            $res->error = "1.1";
+            $res->msg = "UnAuthorized Request Method";
+
+            return new response(json_encode($res));
+
+        }*/
+
+
+        //accueil de la requete
+        $request = Request::createFromGlobals();
+
+        if(!$request){
+
+            $res->error = "2.1";
+            $res->msg = "UnAuthorized Request Method";
+
+            return new response(json_encode($res));
+
+
+        }
+
+
+
+        //users infos
+        $this->user = $this->getUser();
+
+        if(!$this->user){
+
+            $res->error = "1.2";
+            $res->msg = "User Unknown";
+
+            return new response(json_encode($res));
+
+        }
+
+
+
+
+        //SERIALIZED OBJECT
+        $datas = json_decode($request->getContent());
+
+
+//ok
+        if(!$datas || !is_array($datas)){
+
+            $res->error = "2.2";
+            $res->msg = "Datas Corrupted";
+
+            return new response(json_encode($res));
+
+        }
+
+
+        // j'enregistre en base ma note
+        $this->em = $this->getDoctrine()->getManager();
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
