0ea0f35d26eec2be551b8fecd94af2cb853224a1
Guillaume Bailly <g.bailly@immanens.com>
2019-01-01 23:37:58 +0100

    Mise a jour d'engregistrement

--- assets/js/views/frames/home/main.js
+++ assets/js/views/frames/home/main.js
@@ -1,4 +1,7 @@
 import superViews from "../../common/superViews.js";
+
+import Card from "../../common/ui/card.js";
+
 import LoaderCollection from '../../../services/LoaderCollection.js';
 
 export default class Main extends superViews{
@@ -32,7 +35,6 @@ export default class Main extends superViews{
 
 	showList(){
 
-		console.log("in showLiiiiiiiiiiiiiiiiiiiiiiiiiist!!!!!!");
 
 		this.NoteCollection = new LoaderCollection('Note');
 		this.NoteCollection.getAllNotes(this,'addCards');
@@ -42,9 +44,48 @@ export default class Main extends superViews{
 
 	addCards(datas){
 
-		console.log("In Addd Cardsssssssssssssssssss!!!!");
-
 		console.log(datas);
+
+		let card = "";
+		let Elt = "";
+		let item = "";
+
+		let len = datas.rows.length, i;
+		  for (i = 0; i < len; i++) {
+
+
+		  	card = new Card('Card', this.path);
+	
+		    card.setStyle("borderWidth", "0px");
+		    card.setStyle("borderRadius", "0px");
+		    card.setStyle("margin", "0px");
+		    card.setStyle("padding", "10px");
+		    card.setStyle("background", "transparent");
+
+
+			Elt = card.setElement("Element");
+			Elt.setStyle("justifyContent","flex-start");
+
+
+
+				item = card.push("textButton", Elt,"view_Note", "viewNote");
+
+					item.setStylePicto("fontSize","25px");
+					item.setStylePicto("marginRight","0px");
+					item.setStylePicto("color","green");
+					item.setStylePicto("alignItems","center");
+
+					//item.getContainer().addEventListener("click",()=>this.StartNote());
+
+
+
+
+
+
+
+
+
+		  }
 
 
 
