5dae615734312bad7f832f039a5f93a99bf3ffd5
Guillaume Bailly <g.bailly@immanens.com>
2019-01-02 22:16:43 +0100

    Note new new new

--- assets/js/views/frames/note/main.js
+++ assets/js/views/frames/note/main.js
@@ -35,11 +35,11 @@ export default class Main extends superViews{
 
 	initChilds(){
 
-		if(this.parentThis.note.guid == false){
+/*		if(this.parentThis.note.guid == false){
 
 			this.Title     = new Title("Title",this.path);
 		
-		}
+		}*/
 
 			
 
