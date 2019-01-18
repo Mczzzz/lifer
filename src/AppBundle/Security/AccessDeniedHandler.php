97e543ec54ace8cfe00829e39736045e7f99856a
g.bailly <g.bailly@immanens.coms>
2019-01-18 07:01:31 +0100

    Mise a jour de la securité pour passer en full json auth

--- src/AppBundle/Security/AccessDeniedHandler.php
+++ src/AppBundle/Security/AccessDeniedHandler.php
@@ -4,10 +4,9 @@ namespace AppBundle\Security;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Security\Http\Authorization\AuthenticationEntryPointInterface;
 
-class AccessDeniedHandler implements AccessDeniedHandlerInterface
+class AccessDeniedHandler implements AuthenticationEntryPointInterface
 {
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
